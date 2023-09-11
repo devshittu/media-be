7a2f4ac66666228de000f2631911316d4ed5f52e
Muhammed Shittu <devshittu@gmail.com>
2023-09-11 16:50:05 +0100

    Modified for SettingSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -9,10 +9,40 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'email', 'first_name', 'last_name', 'date_joined', 'preferred_categories', 'language', 'theme')
 
-class SettingsSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Settings model.
-    """
-    class Meta:
-        model = Settings
-        fields = ('user', 'preferred_categories', 'language', 'theme')
+class NotificationSettingsDataSerializer(serializers.Serializer):
+    account = serializers.IntegerField()
+    marketing = serializers.IntegerField()
+    updates = serializers.IntegerField()
+
+class SettingNotificationSerializer(serializers.Serializer):
+    email = NotificationSettingsDataSerializer()
+
+class AccountSettingsDataSerializer(serializers.Serializer):
+    display_name = serializers.CharField()
+    email = serializers.EmailField()
+
+class SystemSettingsDataSerializer(serializers.Serializer):
+    theme = serializers.CharField()
+    language = serializers.CharField()
+
+class PersonalSettingsDataSerializer(serializers.Serializer):
+    favorite_categories = serializers.ListField(child=serializers.CharField())
+
+class SettingSerializer(serializers.Serializer):
+    user_id = serializers.UUIDField()
+    system_settings = SystemSettingsDataSerializer()
+    account_settings = AccountSettingsDataSerializer()
+    notification_settings = SettingNotificationSerializer()
+    personal_settings = PersonalSettingsDataSerializer()
+    last_updated = serializers.IntegerField()
+    created_at = serializers.IntegerField()
+    updated_at = serializers.IntegerField()
+
+
+    def validate_settings_data(data):
+        serializer = SettingSerializer(data=data)
+        if serializer.is_valid(raise_exception=True):
+            return serializer.validated_data
+        return None
+
+# users/serializers.py
