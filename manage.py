a4995c7d8ed6fe5c1e675a2e6ad8c225a0300400
Muhammed Shittu <devshittu@gmail.com>
2023-09-12 21:12:29 +0100

    Added load_dotenv library

--- manage.py
+++ manage.py
@@ -2,6 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)), '.env'))
+
 
 
 def main():
