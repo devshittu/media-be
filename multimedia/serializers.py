a5de9d79e03c5e57f9c574c99636b2ea0094f80f
Muhammed Shittu <devshittu@gmail.com>
2023-09-13 22:01:19 +0100

    Added serializers multimedia

---
+++ multimedia/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Multimedia
+
+class MultimediaSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Multimedia model.
+    """
+    user = serializers.ReadOnlyField(source='user.email')  # Display the user's email
+
+    class Meta:
+        model = Multimedia
+        fields = '__all__'
