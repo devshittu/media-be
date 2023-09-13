d4c0bbb3c3e500b0d52ece1448aaff57071fe852
Muhammed Shittu <devshittu@gmail.com>
2023-09-13 22:11:09 +0100

    Refactored serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
-from .models import User, Settings, Category
+from .models import Follow
+from authentication.serializers import UserSerializer
+from authentication.models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
     """
     Serializer for the User model.
     """
     class Meta:
-        model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'date_joined', 'preferred_categories', 'language', 'theme')
+        model = CustomUser
+        fields = ('id', 'email', 'username', 'name', 'date_joined', 'preferred_categories', 'language', 'theme')
 
 class NotificationSettingsDataSerializer(serializers.Serializer):
     account = serializers.IntegerField()
@@ -28,7 +30,7 @@ class SystemSettingsDataSerializer(serializers.Serializer):
 class PersonalSettingsDataSerializer(serializers.Serializer):
     favorite_categories = serializers.ListField(child=serializers.CharField())
 
-class SettingSerializer(serializers.Serializer):
+class UserSettingSerializer(serializers.Serializer):
     user_id = serializers.UUIDField()
     system_settings = SystemSettingsDataSerializer()
     account_settings = AccountSettingsDataSerializer()
@@ -40,9 +42,17 @@ class SettingSerializer(serializers.Serializer):
 
 
     def validate_settings_data(data):
-        serializer = SettingSerializer(data=data)
+        serializer = UserSettingSerializer(data=data)
         if serializer.is_valid(raise_exception=True):
             return serializer.validated_data
         return None
+
+class FollowSerializer(serializers.ModelSerializer):
+    follower = UserSerializer(read_only=True)
+    followed = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Follow
+        fields = ('id', 'follower', 'followed', 'timestamp')
 
 # users/serializers.py
