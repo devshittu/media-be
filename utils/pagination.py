4f3281df9d3589d2bf25b73feca3328062369266
Muhammed Shittu <devshittu@gmail.com>
2023-09-13 21:13:56 +0100

    Added project-wide pagination.

---
+++ utils/pagination.py
@@ -0,0 +1,20 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from .constants import DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, PAGE_SIZE_QUERY_PARAM
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = DEFAULT_PAGE_SIZE
+    page_size_query_param = PAGE_SIZE_QUERY_PARAM
+    max_page_size = MAX_PAGE_SIZE
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'results': data
+        })
