8823adf4d1fceb55a561436aff53265c8a9998ab
Muhammed Shittu <devshittu@gmail.com>
2023-09-14 21:16:15 +0100

    Added generic ActiveUnflaggedManager

--- utils/managers.py
+++ utils/managers.py
@@ -1,5 +1,13 @@
 from django.db import models
 
-class ActiveManager(models.Manager):
+class SoftDeleteManager(models.Manager):
     def active(self):
         return self.filter(deleted_at__isnull=True)
+
+    def deleted(self):
+        return self.filter(deleted_at__isnull=False)
+
+
+class ActiveUnflaggedManager(models.Manager):
+    def active_and_unflagged(self):
+        return self.filter(deleted_at__isnull=True, is_flagged=False)
