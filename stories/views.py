7f993198cc45a0a60f75f2e2c6d8019b4a09d53b
Muhammed Shittu <devshittu@gmail.com>
2023-09-14 21:15:11 +0100

    Added has_liked and bookmark to view

--- stories/views.py
+++ stories/views.py
@@ -1,21 +1,25 @@
 from rest_framework import generics
-from .models import Story, Media, Category, UserInterest
-from .serializers import StorySerializer, MediaSerializer, CategorySerializer, UserInterestSerializer
+from .models import Story,  Like, Dislike, Bookmark
+from .serializers import StorySerializer, BookmarkSerializer, CategorySerializer 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .pagination import CenteredPagination
 from core.settings import ANCESTORS_PER_PAGE, DESCENDANTS_PER_PAGE
+from utils.mixins import SoftDeleteMixin
+from .serializers import LikeSerializer, DislikeSerializer
 
 class StoryListCreateView(generics.ListCreateAPIView):
     """View to list all stories or create a new story."""
     queryset = Story.objects.all()
     serializer_class = StorySerializer
 
-class StoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class StoryRetrieveUpdateDestroyView(SoftDeleteMixin, generics.RetrieveUpdateDestroyAPIView):
     """View to retrieve, update, or delete a story."""
     queryset = Story.objects.all()
     serializer_class = StorySerializer
+    lookup_field = 'slug'  # Use the slug for lookup
+    lookup_url_kwarg = 'story_slug'
 
 
 class StorylineView(APIView):
@@ -37,5 +41,47 @@ class StorylineView(APIView):
         if page is not None:
             serialized_timeline = StorySerializer(page, many=True, context={'leading_slug': story_slug}).data
             return self.pagination_class().get_paginated_response(serialized_timeline)
+
+
+class LikeCreateView(generics.CreateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class DislikeCreateView(generics.CreateAPIView):
+    queryset = Dislike.objects.all()
+    serializer_class = DislikeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class LikeDestroyView(generics.DestroyAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    lookup_field = 'story'
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+class DislikeDestroyView(generics.DestroyAPIView):
+    queryset = Dislike.objects.all()
+    serializer_class = DislikeSerializer
+    lookup_field = 'story'
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+class BookmarkCreateListView(generics.ListCreateAPIView):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class BookmarkRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+
 
 # stories/views.py
