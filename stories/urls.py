ac40884e7f955ff921d4d9c221ee873339107870
Muhammed Shittu <devshittu@gmail.com>
2023-09-14 21:14:45 +0100

    Added has_liked and bookmark to url

--- stories/urls.py
+++ stories/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from . import views
+from .views import StoryListCreateView, StoryRetrieveUpdateDestroyView, StorylineView, LikeCreateView, DislikeCreateView, LikeDestroyView, DislikeDestroyView, BookmarkCreateListView, BookmarkRetrieveUpdateDestroyView
+
+
 
 urlpatterns = [
-    path('stories/', views.StoryListCreateView.as_view(), name='story-list-create'),
-    path('stories/<int:pk>/', views.StoryRetrieveUpdateDestroyView.as_view(), name='story-retrieve-update-destroy'),
-    path('stories/timeline/<slug:story_slug>/', views.StorylineView.as_view(), name='story-timeline'),
-    
+    path('stories/', StoryListCreateView.as_view(), name='story-list-create'),
+    path('stories/<slug:story_slug>/', StoryRetrieveUpdateDestroyView.as_view(), name='story-retrieve-update-destroy'),
+    path('stories/timeline/<slug:story_slug>/', StorylineView.as_view(), name='story-timeline'),
+    path('stories/<int:story_id>/like/', LikeCreateView.as_view(), name='like-story'),
+    path('stories/<int:story_id>/dislike/', DislikeCreateView.as_view(), name='dislike-story'),
+    path('stories/<int:story_id>/unlike/', LikeDestroyView.as_view(), name='unlike-story'),
+    path('stories/<int:story_id>/undislike/', DislikeDestroyView.as_view(), name='undislike-story'),
+
     # Add URLs for Media, Category, and UserInterest views.
+    path('bookmarks/', BookmarkCreateListView.as_view(), name='bookmark-list-create'),
+    path('bookmarks/<int:pk>/', BookmarkRetrieveUpdateDestroyView.as_view(), name='bookmark-retrieve-update-destroy'),
+    
 ]
 
 # stories/urls.py
