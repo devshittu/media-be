fcb80eb0dd3319257a18d3eba71ab6581c9fe2b5
Muhammed Shittu <devshittu@gmail.com>
2023-09-18 23:40:34 +0100

    removed signals for create_user_settings

--- users/models.py
+++ users/models.py
@@ -1,31 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.db.models import JSONField
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from authentication.models import CustomUser
 from utils.models import SoftDeletableModel, TimestampedModel
-
-
-def create_default_settings(user):
-    default_settings = {
-        "system_settings": {"theme": "system", "language": "en"},
-        "account_settings": {
-            "display_name": user.username,
-            "email": user.email,
-        },
-        "notification_settings": {
-            "email": {
-                "account": 1,
-                "marketing": 1,
-                "updates": 1,
-            },
-        },
-        "personal_settings": {
-            "favorite_categories": ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'],
-        }
-    }
-    return UserSetting.objects.create(user=user, **default_settings)
 
 class UserSetting(SoftDeletableModel, TimestampedModel):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='settings')
@@ -40,13 +17,6 @@ class UserSetting(SoftDeletableModel, TimestampedModel):
 
     def __str__(self):
         return f"{self.user.email}'s Settings"
-    
-
-    
-@receiver(post_save, sender=CustomUser)
-def create_user_settings(sender, instance, created, **kwargs):
-    if created:
-        create_default_settings(instance)
 
 # TODO: it needs to be only created at not updated at.
 class Follow(SoftDeletableModel, TimestampedModel):
