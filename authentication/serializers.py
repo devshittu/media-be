93eb39231777f6a598f68caa658f72c81b45a9f8
Muhammed Shittu <devshittu@gmail.com>
2023-09-19 21:34:13 +0100

    Added CustomUserRegistrationSerializer and CustomUserSerializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,10 +1,36 @@
 from django.contrib.auth import get_user_model
+from utils.serializers import UnixTimestampDateTimeField
+from .models import CustomUser
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the custom user model.
-    """
+
+class CustomUserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'name',  'username', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):
+        user = CustomUser(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            username=validated_data.get('username', None) or f"user_{validated_data['email'].split('@')[0]}"
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+# class CustomUserSerializer(UnixTimestampDateTimeField):
+class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'name', 'profile_picture', 'date_of_birth', 'bio', 'phone_number', 'last_activity', 'has_completed_setup', 'roles')
+        fields = [
+            'id', 'name', 'email', 'username', 'avatar_url', 'display_picture',
+            'date_of_birth', 'bio', 'phone_number', 'last_activity', 'roles',
+            'is_active', 'is_staff', 'has_completed_setup'
+        ]
+
+# authentication/serializers.py
