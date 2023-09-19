17bfa82bfcfa490f3a9eca9ca5ab482152c7e8c4
Muhammed Shittu <devshittu@gmail.com>
2023-09-19 20:46:11 +0100

    Added UpdateFeedPositionView url

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import UserListCreateView, UserRetrieveUpdateDestroyView, UserSettingListCreateView, FollowUserView, UnfollowUserView, UserFollowersListView
+from .views import (UserListCreateView, UserRetrieveUpdateDestroyView, UserSettingListCreateView, 
+                    FollowUserView, UnfollowUserView, UserFollowersListView, UpdateFeedPositionView)
 
 urlpatterns = [
     path('users/', UserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-retrieve-update-destroy'),
     path('settings/', UserSettingListCreateView.as_view(), name='user-settings-list-create'),
+
+    path('update-feed-position/', UpdateFeedPositionView.as_view(), name='update-feed-position'),
 
     path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
     path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
