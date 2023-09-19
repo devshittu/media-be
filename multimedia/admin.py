373be6b953e8ade48ccd11f36c9ba352da0fe16d
Muhammed Shittu <devshittu@gmail.com>
2023-09-19 21:16:11 +0100

    Added MultimediaAdmin

--- multimedia/admin.py
+++ multimedia/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Multimedia
 
-# Register your models here.
+class MultimediaAdmin(admin.ModelAdmin):
+    list_display = ('media_type', 'user', 'story', 'created_at', 'updated_at')
+    list_filter = ('media_type', 'user', 'story', 'created_at', 'updated_at')
+    search_fields = ('media_type', 'user__email', 'story__id')
+    ordering = ('-created_at',)
+
+admin.site.register(Multimedia, MultimediaAdmin)
