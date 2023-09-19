980a14eece8e767728246b1e4ad75a08efa565af
Muhammed Shittu <devshittu@gmail.com>
2023-09-19 21:01:45 +0100

    Added UnixTimestampModelSerializer

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,27 +1,36 @@
 from rest_framework import serializers
 from .models import Story, Category, Like, Dislike, Bookmark
+from utils.serializers import UnixTimestampModelSerializer
 from multimedia.serializers import MultimediaSerializer
+from authentication.serializers import CustomUserSerializer
 
+class CategorySerializer(UnixTimestampModelSerializer):
+    slug = serializers.SlugField(read_only=True)
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    """Serializer for the Category model."""
     class Meta:
         model = Category
         fields = '__all__'
 
-class StorySerializer(serializers.ModelSerializer):
+    def validate_title(self, value):
+        if Category.objects.filter(title=value).exists():
+            raise serializers.ValidationError("A category with this title already exists.")
+        return value
+
+
+class StorySerializer(UnixTimestampModelSerializer):
     """Serializer for the Story model."""
-    media = MultimediaSerializer(many=True, read_only=True)
-    lineage = serializers.SerializerMethodField()
     has_liked = serializers.SerializerMethodField()
-    bookmarked = serializers.SerializerMethodField()
+    has_bookmarked = serializers.SerializerMethodField()
+    user = CustomUserSerializer(read_only=True)
+    multimedia = MultimediaSerializer(many=True, read_only=True)
+    total_likes = serializers.IntegerField(read_only=True)
+    total_dislikes = serializers.IntegerField(read_only=True)
 
 
     class Meta:
         model = Story
-        # fields = '__all__'
-        fields = ['id', 'title', 'slug', 'body', 'has_liked', 'likes_count', 'dislikes_count', 'bookmarked', 'category', 'parents', 'children', 'source_link', 'event_occurred_at', 'event_reported_at', 'created_at', 'updated_at'  ]  # other fields
+        fields = '__all__'
+        # fields = ['id', 'title', 'slug', 'body', 'has_liked', 'likes_count', 'dislikes_count', 'bookmarked', 'category', 'parents', 'children', 'source_link', 'event_occurred_at', 'event_reported_at', 'created_at', 'updated_at'  ]  # other fields
 
     def get_has_liked(self, obj):
         user = self.context['request'].user
@@ -29,31 +38,25 @@ class StorySerializer(serializers.ModelSerializer):
             return Like.objects.filter(story=obj, user=user).exists()
         return None
     
-    def get_bookmarked(self, obj):
+    def get_has_bookmarked(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
             return Bookmark.objects.filter(story=obj, user=user).exists()
         return None
 
 
-# class UserInterestSerializer(serializers.ModelSerializer):
-#     """Serializer for the UserInterest model."""
-#     class Meta:
-#         model = UserInterest
-#         fields = '__all__'
 
-
-class LikeSerializer(serializers.ModelSerializer):
+class LikeSerializer(UnixTimestampModelSerializer):
     class Meta:
         model = Like
         fields = ['user', 'story', 'created_at']
 
-class DislikeSerializer(serializers.ModelSerializer):
+class DislikeSerializer(UnixTimestampModelSerializer):
     class Meta:
         model = Dislike
         fields = ['user', 'story', 'created_at']
 
-class BookmarkSerializer(serializers.ModelSerializer):
+class BookmarkSerializer(UnixTimestampModelSerializer):
     class Meta:
         model = Bookmark
         fields = ['id', 'user', 'story', 'created_at']
