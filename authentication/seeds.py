fd8684cf52603ceeff3a50834730c63af3a4c49e
Muhammed Shittu <devshittu@gmail.com>
2023-09-19 21:34:36 +0100

    Added CustomUserSeed

---
+++ authentication/seeds.py
@@ -0,0 +1,54 @@
+from autoseed.utils.base_seed import BaseSeed
+from .models import CustomUser
+from django.utils import timezone
+from utils.helpers import unix_to_datetime
+import random
+from datetime import timedelta
+import datetime
+from django.contrib.auth.hashers import make_password
+
+
+
+class CustomUserSeed(BaseSeed):
+    raw_file = 'users'
+    model = CustomUser
+
+    @classmethod
+    def get_fields(cls, item):
+
+        hashed_password = make_password(item['password'])
+
+        # Convert Unix timestamp to timezone-aware datetime
+        print(f"Converting last_login: {item.get('last_login')}")
+        last_login_time = unix_to_datetime(item.get('last_login')) if item.get('last_login') else None
+
+
+        # Random Date of Birth
+        end_date = timezone.now().date() - timedelta(days=18*365)  # 18 years ago
+        start_date = end_date - timedelta(days=52*365)  # 70 years ago
+        random_dob = start_date + timedelta(days=random.randint(0, (end_date - start_date).days))
+
+        # Random Bio
+        bios = [
+            "I love reading and traveling.",
+            "A passionate writer and coffee lover.",
+            "Always looking for the next adventure."
+        ]
+        random_bio = random.choice(bios)
+
+        # Random has_completed_setup
+        random_setup = random.choice([True, False])
+
+        return {
+            'name': item['name'],
+            'username': item['username'],
+            'email': item['email'],
+            'password': hashed_password,
+            'roles': item['roles'],
+            'date_of_birth': random_dob.isoformat() if isinstance(random_dob, datetime.date) else random_dob,
+            'bio': random_bio,
+            'has_completed_setup': random_setup,
+            'last_activity': last_login_time.isoformat() if last_login_time else None,
+        }
+
+# authentication/seeds.py
