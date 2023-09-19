0b8f2b13e7ccc6287969cbecceac9618dc37731c
Muhammed Shittu <devshittu@gmail.com>
2023-09-19 20:51:57 +0100

    Added UserFeed url

--- stories/urls.py
+++ stories/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
-from .views import StoryListCreateView, StoryRetrieveUpdateDestroyView, StorylineView, LikeCreateView, DislikeCreateView, LikeDestroyView, DislikeDestroyView, BookmarkCreateListView, BookmarkRetrieveUpdateDestroyView
+from .views import (CategoryListCreateView, CategoryRetrieveUpdateDestroyView, 
+                    StoryListCreateView, StoryRetrieveUpdateDestroyView, StorylineView, 
+                    UserFeedView, 
+                    LikeCreateView, DislikeCreateView, LikeDestroyView, 
+                    DislikeDestroyView, BookmarkCreateListView, BookmarkRetrieveUpdateDestroyView,
+                    )
 
 
 
 urlpatterns = [
+    path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('categories/<slug:slug>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-retrieve-update-destroy'),
     path('stories/', StoryListCreateView.as_view(), name='story-list-create'),
+    path('user-feed/', UserFeedView.as_view(), name='user-feed'),
     path('stories/<slug:story_slug>/', StoryRetrieveUpdateDestroyView.as_view(), name='story-retrieve-update-destroy'),
-    path('stories/timeline/<slug:story_slug>/', StorylineView.as_view(), name='story-timeline'),
+    path('stories/<slug:story_slug>/timeline/', StorylineView.as_view(), name='story-timeline'),
     path('stories/<int:story_id>/like/', LikeCreateView.as_view(), name='like-story'),
     path('stories/<int:story_id>/dislike/', DislikeCreateView.as_view(), name='dislike-story'),
     path('stories/<int:story_id>/unlike/', LikeDestroyView.as_view(), name='unlike-story'),
