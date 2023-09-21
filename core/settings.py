9e7eb2dd59c07ed966b56b0bd2b570bbb479d393
Muhammed Shittu <devshittu@gmail.com>
2023-09-21 07:29:22 +0100

    Installed django_extensions in file

--- core/settings.py
+++ core/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
+    'django_extensions',
 
     'ckeditor',
     # 'ckeditor_uploader',  # If you want image uploading
