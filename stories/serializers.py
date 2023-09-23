7b4d2f5fed0bddc140e1c236f60b5a8a2748ff40
Muhammed Shittu <devshittu@gmail.com>
2023-09-23 16:23:37 +0100

    Added storylines_count and get_storylines_count to storyserializers

--- stories/serializers.py
+++ stories/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Story, Category, Like, Dislike, Bookmark
+from .neo_models import StoryNode
 from utils.serializers import UnixTimestampModelSerializer
 from multimedia.serializers import MultimediaSerializer
 from authentication.serializers import CustomUserSerializer
@@ -25,13 +26,17 @@ class StorySerializer(UnixTimestampModelSerializer):
     multimedia = MultimediaSerializer(many=True, read_only=True)
     total_likes = serializers.IntegerField(read_only=True)
     total_dislikes = serializers.IntegerField(read_only=True)
+    storylines_count = serializers.SerializerMethodField()
+    category = CategorySerializer(read_only=True)
 
 
     class Meta:
         model = Story
-        fields = '__all__'
-        # fields = ['id', 'title', 'slug', 'body', 'has_liked', 'likes_count', 'dislikes_count', 'bookmarked', 'category', 'parents', 'children', 'source_link', 'event_occurred_at', 'event_reported_at', 'created_at', 'updated_at'  ]  # other fields
-
+        # fields = '__all__'
+        fields = ['storylines_count', 'has_liked', 'has_bookmarked', 'user', 'multimedia', 'total_likes', 'total_dislikes'] + [f.name for f in Story._meta.fields]
+        # if there are fields from the model that we don't want to show in the view here it is 'slug', 'body', 'deleted_at'.
+        # fields = ['storylines_count', 'multimedia'] + [f.name for f in Story._meta.fields if f.name not in ['slug', 'body', 'deleted_at']]
+       
     def get_has_liked(self, obj):
         user = self.context['request'].user
         if user.is_authenticated:
@@ -43,6 +48,17 @@ class StorySerializer(UnixTimestampModelSerializer):
         if user.is_authenticated:
             return Bookmark.objects.filter(story=obj, user=user).exists()
         return None
+    
+    def get_storylines_count(self, obj):
+        try:
+            # Get the StoryNode from Neo4j
+            story_node = StoryNode.nodes.get(story_id=obj.id)
+            
+            # Get all Storyline nodes this story belongs to
+            storylines = list(story_node.belongs_to_storyline.all())
+            return len(storylines)
+        except:
+            return 0
 
 
 
