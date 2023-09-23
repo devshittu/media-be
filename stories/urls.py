ed28121558679fa6f11613333270e8a6916863cf
Muhammed Shittu <devshittu@gmail.com>
2023-09-23 16:21:03 +0100

    Added urls for storylines story by categories and storyline by story slug

--- stories/urls.py
+++ stories/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import (CategoryListCreateView, CategoryRetrieveUpdateDestroyView, 
-                    StoryListCreateView, StoryRetrieveUpdateDestroyView, StorylineView, 
-                    UserFeedView, 
+                    StoryListCreateView, StoryRetrieveUpdateDestroyView, 
+                    UserFeedView, StoriesByCategoryView,
                     LikeCreateView, DislikeCreateView, LikeDestroyView, 
                     DislikeDestroyView, BookmarkCreateListView, BookmarkRetrieveUpdateDestroyView,
                     )
+from .neo_views import StorylineView, StorylineListView, StorylinesForStoryView
 
 
 
@@ -12,13 +13,19 @@ urlpatterns = [
     path('categories/', CategoryListCreateView.as_view(), name='category-list-create'),
     path('categories/<slug:slug>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-retrieve-update-destroy'),
     path('stories/', StoryListCreateView.as_view(), name='story-list-create'),
+    path('stories/category/<slug:category_slug>/', StoriesByCategoryView.as_view(), name='stories-by-category'),
     path('user-feed/', UserFeedView.as_view(), name='user-feed'),
     path('stories/<slug:story_slug>/', StoryRetrieveUpdateDestroyView.as_view(), name='story-retrieve-update-destroy'),
-    path('stories/<slug:story_slug>/timeline/', StorylineView.as_view(), name='story-timeline'),
+    
     path('stories/<int:story_id>/like/', LikeCreateView.as_view(), name='like-story'),
     path('stories/<int:story_id>/dislike/', DislikeCreateView.as_view(), name='dislike-story'),
     path('stories/<int:story_id>/unlike/', LikeDestroyView.as_view(), name='unlike-story'),
     path('stories/<int:story_id>/undislike/', DislikeDestroyView.as_view(), name='undislike-story'),
+    
+    path('stories/<slug:slug>/storylines/', StorylinesForStoryView.as_view(), name='story-storylines'),
+
+    path('storylines/', StorylineListView.as_view(), name='storyline-list'),
+    path('storylines/<str:storyline_id>/', StorylineView.as_view(), name='storyline-detail'),
 
     # Add URLs for Media, Category, and UserInterest views.
     path('bookmarks/', BookmarkCreateListView.as_view(), name='bookmark-list-create'),
