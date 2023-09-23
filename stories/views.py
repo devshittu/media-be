65a1277496e2ffbb1c78f686ec99fa20e1597ade
Muhammed Shittu <devshittu@gmail.com>
2023-09-23 16:19:14 +0100

    Added StoriesByCategoryView

--- stories/views.py
+++ stories/views.py
@@ -4,13 +4,9 @@ from .models import Story,  Like, Dislike, Bookmark, Category
 from .serializers import StorySerializer, BookmarkSerializer, CategorySerializer 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .pagination import CenteredPagination
-from core.settings import ANCESTORS_PER_PAGE, DESCENDANTS_PER_PAGE
 from utils.mixins import SoftDeleteMixin
 from .serializers import LikeSerializer, DislikeSerializer, CategorySerializer
-from users.models import UserSetting
 from utils.pagination import CustomPageNumberPagination
 
 class CategoryListCreateView(generics.ListCreateAPIView):
@@ -25,6 +21,16 @@ class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'slug'
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class StoriesByCategoryView(generics.ListAPIView):
+    queryset = Story.active_unflagged_objects.all()
+    serializer_class = StorySerializer
+    pagination_class = CustomPageNumberPagination
+
+    def get_queryset(self):
+        category_slug = self.kwargs.get('category_slug')
+        return self.queryset.filter(category__slug=category_slug)
 
 
 class StoryListCreateView(generics.ListCreateAPIView):
@@ -48,61 +54,7 @@ class StoryRetrieveUpdateDestroyView(SoftDeleteMixin, generics.RetrieveUpdateDes
     lookup_url_kwarg = 'story_slug'
 
 
-
-# from django.db.models import Q
-
-# class StorylineView(APIView):
-#     pagination_class = CenteredPagination
-
-#     def get(self, request, story_slug):
-#         print("Received slug:", story_slug)
-#         all_slugs = Story.objects.values_list('slug', flat=True)
-#         print("All slugs in database:", all_slugs)
-#         try:
-#             leading_story = Story.objects.get(slug=story_slug)
-#         except Story.DoesNotExist:
-#             return Response({"error": "Story not found."}, status=status.HTTP_404_NOT_FOUND)
-
-#         # Fetch the leading story, its ancestors, and its descendants based on configuration
-#         ancestors = leading_story.get_all_parents()[:ANCESTORS_PER_PAGE]
-#         descendants = leading_story.get_all_children()[:DESCENDANTS_PER_PAGE]
-#         timeline_ids = [story.id for story in ancestors + [leading_story] + descendants]
-
-#         # Convert the list of stories back to a queryset
-#         timeline_queryset = Story.objects.filter(id__in=timeline_ids)
-
-#         # Paginate the timeline
-#         page = self.pagination_class().paginate_queryset(timeline_queryset, request, view=self)
-#         if page is not None:
-#             serialized_timeline = StorySerializer(page, many=True, context={'leading_slug': story_slug}).data
-#             return self.pagination_class().get_paginated_response(serialized_timeline)
-
-class StorylineView(APIView):
-    pagination_class = CenteredPagination
-
-    def get(self, request, story_slug):
-        print(f"Inside StorylineView.get with slug: {story_slug}")
-
-        try:
-            leading_story = Story.objects.get(slug=story_slug)
-            print(f"Found story with title: {leading_story.title}")
-        except Story.DoesNotExist:
-            print("Error: Story not found in direct query.")
-            return Response({"error": "Story not found."}, status=status.HTTP_404_NOT_FOUND)
-
-        ancestors = leading_story.get_all_parents()[:ANCESTORS_PER_PAGE]
-        descendants = leading_story.get_all_children()[:DESCENDANTS_PER_PAGE]
-        timeline = ancestors + [leading_story] + descendants
-
-        print(f"Ancestors count: {len(ancestors)}, Descendants count: {len(descendants)}, Total timeline count: {len(timeline)}")
-
-        page = self.pagination_class().paginate_queryset(timeline, request, view=self)
-        if page is not None:
-            serialized_timeline = StorySerializer(page, many=True, context={'leading_slug': story_slug}).data
-            return self.pagination_class().get_paginated_response(serialized_timeline)
-        else:
-            print("Error: Page is None after pagination.")
-            return Response({"error": "Pagination error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#             return Response({"error": "Pagination error."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class UserFeedView(APIView):
