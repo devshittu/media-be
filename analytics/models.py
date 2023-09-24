1c21f7b3788a71e974b0edafbfd4e64202e60780
Muhammed Shittu <devshittu@gmail.com>
2023-09-24 20:15:51 +0100

    Refactored analytics models

--- analytics/models.py
+++ analytics/models.py
@@ -3,63 +3,23 @@ from django.conf import settings
 from django.db.models import JSONField
 from utils.models import SoftDeletableModel, TimestampedModel
 
-class DeviceData(SoftDeletableModel, TimestampedModel):
-    """
-    Model to track device and technical data.
-    """
-    interaction = models.OneToOneField('StoryInteraction', on_delete=models.CASCADE, related_name='device_data')
-    device_type = models.CharField(max_length=50)
-    operating_system = models.CharField(max_length=50)
-    browser = models.CharField(max_length=50)
-    screen_resolution = models.CharField(max_length=50)
-    connection_type = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = 'Device Data'
-        verbose_name_plural = 'Device Data'
-
-    def __str__(self):
-        return f"Device data for interaction: {self.interaction.id}"
-
-class LocationData(SoftDeletableModel, TimestampedModel):
-    """
-    Model to track user location data.
-    """
-    interaction = models.OneToOneField('StoryInteraction', on_delete=models.CASCADE, related_name='location_data')
-    ip_address = models.GenericIPAddressField()
-    geolocation = models.CharField(max_length=255)  # This could be further normalized if needed
-    time_zone = models.CharField(max_length=50)
-
-    class Meta:
-        verbose_name = 'Location Data'
-        verbose_name_plural = 'Locations Data'
-
-    def __str__(self):
-        return f"Location data for interaction: {self.interaction.id}"
-
-class ReferralData(SoftDeletableModel, TimestampedModel):
-    """
-    Model to track referral data.
-    """
-    interaction = models.OneToOneField('StoryInteraction', on_delete=models.CASCADE, related_name='referral_data')
-    referrer_url = models.URLField()
-    search_terms = models.TextField(blank=True, null=True)
-
-    class Meta:
-        verbose_name = 'Deferral Data'
-        verbose_name_plural = 'Deferrals Data'
-
-    def __str__(self):
-        return f"Referral data for interaction: {self.interaction.id}"
-
 class UserSession(SoftDeletableModel, TimestampedModel):
     """
     Model to represent a user's session.
     """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sessions')
-    session_id = models.UUIDField(unique=True)  # Unique identifier for the session
-    device_data = models.OneToOneField(DeviceData, on_delete=models.CASCADE, related_name='session')
-    location_data = models.OneToOneField(LocationData, on_delete=models.CASCADE, related_name='session')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_sessions')
+    session_token = models.UUIDField(unique=True)  # Unique identifier for the session
+    # location_data 
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+    geolocation = models.CharField(max_length=255, null=True, blank=True)  # This could be further normalized if needed
+    time_zone = models.CharField(max_length=50, null=True, blank=True)
+    # device_data 
+    device_type = models.CharField(max_length=50, null=True, blank=True)
+    operating_system = models.CharField(max_length=50, null=True, blank=True)
+    browser = models.CharField(max_length=50, null=True, blank=True)
+    screen_resolution = models.CharField(max_length=50, null=True, blank=True)
+    connection_type = models.CharField(max_length=50, null=True, blank=True)
+
     start_timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -68,6 +28,7 @@ class UserSession(SoftDeletableModel, TimestampedModel):
 
     def __str__(self):
         return f"Session {self.session_id} for {self.user}"
+
 
 class StoryInteraction(SoftDeletableModel, TimestampedModel):
     """
@@ -79,7 +40,9 @@ class StoryInteraction(SoftDeletableModel, TimestampedModel):
         ('unbookmark', 'Unbookmark'),
         ('share', 'Share'),
         ('click_external', 'Click External Link'),
-        ('view_timeline', 'View Story Timeline'),
+        ('view_storyline', 'View Storyline'),
+        ('report', 'Report story'),
+        ('highlight_text', 'Highlight Text'),
         # ... other interactions ...
     ]
 
@@ -87,17 +50,24 @@ class StoryInteraction(SoftDeletableModel, TimestampedModel):
     story = models.ForeignKey('stories.Story', on_delete=models.CASCADE, related_name='interactions')
     interaction_type = models.CharField(max_length=50, choices=INTERACTION_CHOICES)
     metadata = JSONField(blank=True, null=True)  # Additional data for the interaction
-    session = models.ForeignKey(UserSession, on_delete=models.CASCADE, related_name='interactions')
-    #     scroll_depth = models.PositiveIntegerField(null=True)  # Represented as a percentage
-#     highlighted_text = models.TextField(blank=True, null=True)
-    
+    user_session = models.ForeignKey(UserSession, on_delete=models.CASCADE, related_name='interactions')
 
     class Meta:
         verbose_name = 'Story Interaction'
         verbose_name_plural = 'Stories Interactions'
 
     def __str__(self):
-        return f"{self.user} {self.interaction_type} {self.story} on {self.timestamp}"
+        return f"{self.user} {self.interaction_type} {self.story} on {self.created_at}"
+
+
+class StoryInteractionMetadataSchema(SoftDeletableModel, TimestampedModel):
+    interaction_type = models.CharField(max_length=50, choices=StoryInteraction.INTERACTION_CHOICES)
+    version = models.CharField(max_length=10)
+    schema = JSONField()
+
+    class Meta:
+        unique_together = ('interaction_type', 'version')
+
 
 class UserNotInterested(SoftDeletableModel, TimestampedModel):
     """
@@ -113,7 +83,7 @@ class UserNotInterested(SoftDeletableModel, TimestampedModel):
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"{self.user} not interested in {self.story} on {self.timestamp}"
+        return f"{self.user} not interested in {self.story} on {self.created_at}"
 
 
 class AccessibilityTool(SoftDeletableModel, TimestampedModel):
@@ -134,5 +104,6 @@ class AccessibilityTool(SoftDeletableModel, TimestampedModel):
         verbose_name_plural = 'Accessibility Tools'
 
     def __str__(self):
-        return f"{self.user} used {self.tool_name} on {self.timestamp}"
+        return f"{self.user} used {self.tool_name} on {self.created_at}"
+
 # analytics/models.py
