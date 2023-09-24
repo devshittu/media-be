02933e8534503604c7a2a1b4722a11841ed562d5
Muhammed Shittu <devshittu@gmail.com>
2023-09-24 20:15:36 +0100

    Refactored analytics admin

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,30 +1,16 @@
 from django.contrib import admin
 from .models import (
-    DeviceData, LocationData, ReferralData, UserSession, 
+     UserSession, 
     StoryInteraction, UserNotInterested, AccessibilityTool
 )
 
-class DeviceDataAdmin(admin.ModelAdmin):
-    list_display = ('interaction', 'device_type', 'operating_system', 'browser')
-    search_fields = ('device_type', 'operating_system', 'browser')
-    list_filter = ('device_type', 'operating_system', 'browser')
-
-class LocationDataAdmin(admin.ModelAdmin):
-    list_display = ('interaction', 'ip_address', 'geolocation', 'time_zone')
-    search_fields = ('ip_address', 'geolocation', 'time_zone')
-    list_filter = ('geolocation', 'time_zone')
-
-class ReferralDataAdmin(admin.ModelAdmin):
-    list_display = ('interaction', 'referrer_url', 'search_terms')
-    search_fields = ('referrer_url', 'search_terms')
-
 class UserSessionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'session_id', 'start_timestamp')
-    search_fields = ('user__username', 'session_id')
+    list_display = ('user', 'start_timestamp', 'device_type', 'operating_system', 'browser', 'ip_address', 'geolocation', 'time_zone')
+    search_fields = ('user__username', 'device_type', 'operating_system', 'browser', 'ip_address', 'geolocation', 'time_zone')
     list_filter = ('start_timestamp',)
 
 class StoryInteractionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'story', 'interaction_type', 'session')
+    list_display = ('user', 'story', 'interaction_type')
     search_fields = ('user__username', 'story__title', 'interaction_type')
     list_filter = ('interaction_type',)
 
@@ -38,9 +24,6 @@ class AccessibilityToolAdmin(admin.ModelAdmin):
     search_fields = ('user__username', 'tool_name')
     list_filter = ('tool_name',)
 
-admin.site.register(DeviceData, DeviceDataAdmin)
-admin.site.register(LocationData, LocationDataAdmin)
-admin.site.register(ReferralData, ReferralDataAdmin)
 admin.site.register(UserSession, UserSessionAdmin)
 admin.site.register(StoryInteraction, StoryInteractionAdmin)
 admin.site.register(UserNotInterested, UserNotInterestedAdmin)
