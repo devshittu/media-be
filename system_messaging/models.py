14a80a55bf1e24da352476321a5b8cec5bb487e3
Muhammed Shittu <devshittu@gmail.com>
2023-09-25 22:55:06 +0100

    Added path

--- system_messaging/models.py
+++ system_messaging/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.conf import settings
 from jinja2.sandbox import SandboxedEnvironment
+from utils.models import SoftDeletableModel, TimestampedModel
 
 
-class MessageTemplate(models.Model):
+class MessageTemplate(SoftDeletableModel, TimestampedModel):
     MESSAGE_TYPES = (
-        ('email', 'Email'),
-        ('sms', 'Short Messaging Service'),
+        ("email", "Email"),
+        ("sms", "Short Messaging Service"),
     )
 
-    message_type = models.CharField(max_length=5, choices=MESSAGE_TYPES, default='email')
+    message_type = models.CharField(
+        max_length=5, choices=MESSAGE_TYPES, default="email"
+    )
     code = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
     subject = models.TextField(blank=True, null=True)  # SMS might not need a subject
@@ -20,7 +23,7 @@ class MessageTemplate(models.Model):
     class Meta:
         verbose_name = "Message Template"
         verbose_name_plural = "Message Templates"
-    
+
     def replace_placeholders(self, context):
         """Render email body and subject using Jinja2 from database content with sandboxing."""
 
@@ -29,8 +32,11 @@ class MessageTemplate(models.Model):
         # Extract templates from the database and render them with Jinja2
         template_body = jinja_env.from_string(self.body)
         rendered_body = template_body.render(**context)
-        
+
         template_subject = jinja_env.from_string(self.subject)
         rendered_subject = template_subject.render(**context)
-        
+
         return rendered_subject, rendered_body
+
+
+# system_messaging/models.py
