87575688fadfd83056fe6a724801bed72a0dae9f
Muhammed Shittu <devshittu@gmail.com>
2023-09-25 22:57:06 +0100

    Added seed for UserFeedPositionSeed

--- users/seeds.py
+++ users/seeds.py
@@ -1,23 +1,22 @@
 # UserFeedPosition
+from django.utils import timezone
 from autoseed.utils.base_seed import BaseSeed
 from .models import UserFeedPosition
-from django.utils import timezone
-from utils.helpers import unix_to_datetime
+
 
 class UserFeedPositionSeed(BaseSeed):
-    raw_file = 'user_feed_position'
+    raw_file = "user_feed_position"
     model = UserFeedPosition
 
     @classmethod
     def get_fields(cls, item):
-
         # Use timezone-aware datetime
         current_time = timezone.now().isoformat()
         return {
-            'user': item['user'],
-            'last_story_read': item['last_story_read'],
-            'created_at': current_time,
-            'updated_at': current_time
+            "user": item["user"],
+            "last_story_read": item["last_story_read"],
+            "created_at": current_time,
+            "updated_at": current_time,
         }
 
 
