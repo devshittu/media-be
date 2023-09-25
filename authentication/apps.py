9522f994ec34488df12b2c08a9f2290ea057e9e4
Muhammed Shittu <devshittu@gmail.com>
2023-09-25 22:10:10 +0100

    Added signals for authentication

--- authentication/apps.py
+++ authentication/apps.py
@@ -1,6 +1,11 @@
 from django.apps import AppConfig
+import authentication
 
 
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
+
+    def ready(self):
+        import authentication.signals
+
