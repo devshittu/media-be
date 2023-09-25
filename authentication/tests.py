b8aa9ca7847fb4c6b33a13f81d10e0b87770a79e
Muhammed Shittu <devshittu@gmail.com>
2023-09-25 22:36:02 +0100

    Added registration test

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import CustomUser
 
-# Create your tests here.
+class RegistrationTestCase(APITestCase):
+
+    def test_registration(self):
+        data = {
+            "email": "testuser@test.com",
+            "username": "testuser",
+            "password": "strong_password_123",
+            "name": "Test User"
+        }
+        response = self.client.post(reverse('name-of-registration-view'), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(CustomUser.objects.count(), 1)
