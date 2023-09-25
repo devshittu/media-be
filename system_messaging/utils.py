63e4ddfb1b33aacc2b77158b31620a33d44372ee
Muhammed Shittu <devshittu@gmail.com>
2023-09-25 22:55:53 +0100

    Formatted

--- system_messaging/utils.py
+++ system_messaging/utils.py
@@ -2,7 +2,9 @@ from .models import MessageTemplate
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import MessageTemplate
+
 # from twilio.rest import Client
+
 
 def send_sms(recipient, message):
     """Send an SMS via Twilio."""
@@ -14,6 +16,7 @@ def send_sms(recipient, message):
     # )
     pass
 
+
 def send_message(template_code, context, recipient):
     try:
         template = MessageTemplate.objects.get(code=template_code)
@@ -21,16 +24,21 @@ def send_message(template_code, context, recipient):
         print(f"Message template with code {template_code} does not exist.")
         return
 
-    required_variables = template.variables.split(',')
+    required_variables = template.variables.split(",")
     missing_variables = [var for var in required_variables if var not in context]
-    
+
     if missing_variables:
-        print(f"Error sending message: Missing context variables: {', '.join(missing_variables)}")
+        print(
+            f"Error sending message: Missing context variables: {', '.join(missing_variables)}"
+        )
         return
 
     subject, message = template.replace_placeholders(context)
 
-    if template.message_type == 'email':
-        send_mail(subject, message, 'from_email@example.com', [recipient])
-    elif template.message_type == 'sms':
+    if template.message_type == "email":
+        send_mail(subject, message, "from_email@example.com", [recipient])
+    elif template.message_type == "sms":
         send_sms(recipient, message)
+
+
+# system_messaging/utils.py
