ca4d11c1df1548c895e8ffa94a35a0ee8c3d6e9e
Muhammed Shittu <devshittu@gmail.com>
2023-09-25 22:55:35 +0100

    Added seed for MessageTemplate

---
+++ system_messaging/seeds.py
@@ -0,0 +1,24 @@
+from django.utils import timezone
+from autoseed.utils.base_seed import BaseSeed
+from .models import MessageTemplate
+
+
+class MessageTemplateSeed(BaseSeed):
+    raw_file = "message_templates"
+    model = MessageTemplate
+
+    @classmethod
+    def get_fields(cls, item):
+        # Use timezone-aware datetime
+        current_time = timezone.now().isoformat()
+        return {
+            "message_type": item["message_type"],
+            "code": item["code"],
+            "description": item["description"],
+            "subject": item["subject"],
+            "body": item["body"],
+            "user": item["user"],
+            "variables": item["variables"],
+            "created_at": current_time,
+            "updated_at": current_time,
+        }
