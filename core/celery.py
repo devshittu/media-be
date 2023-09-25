81d7ca69a7fbef0ff885b53beba5d087e0d2dbbe
Muhammed Shittu <devshittu@gmail.com>
2023-09-25 22:52:08 +0100

    Setting up celery

---
+++ core/celery.py
@@ -0,0 +1,14 @@
+import os
+from celery import Celery
+from django.conf import settings
+from decouple import config
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# app = Celery('media-be')
+app = Celery(config('APP_CODE_NAME', default='appname-media-be', cast=str))
+
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
