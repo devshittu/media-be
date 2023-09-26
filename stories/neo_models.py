534858adde1b035d3a2eaca01a0c3107634eedd3
Muhammed Shittu <devshittu@gmail.com>
2023-09-26 12:40:55 +0100

    create hashtag node

--- stories/neo_models.py
+++ stories/neo_models.py
@@ -12,6 +12,7 @@ class StoryNode(DjangoNode):
     belongs_to_storyline = RelationshipTo('Storyline', 'PART_OF')
     next_story = RelationshipTo('StoryNode', 'FOLLOWS')
     previous_story = RelationshipFrom('StoryNode', 'FOLLOWS')
+    hashtags = RelationshipTo('Hashtag', 'HAS_HASHTAG')
 
 class Storyline(DjangoNode):
     id = UniqueIdProperty()
@@ -24,3 +25,9 @@ class Storyline(DjangoNode):
     @property
     def total_stories(self):
         return len(self.stories.all())
+    
+class Hashtag(DjangoNode):
+    name = StringProperty(unique_index=True)
+    stories = RelationshipFrom('StoryNode', 'HAS_HASHTAG')
+
+# stories/neo_models.py
