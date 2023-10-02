f871fb3fb10edd68029dc6ad7d42eb5b4fd5612b
Muhammed Shittu <devshittu@gmail.com>
2023-10-02 04:04:47 +0100

    Refactored to used dependency

--- users/seeds.py
+++ users/seeds.py
@@ -2,11 +2,17 @@
 from django.utils import timezone
 from managekit.utils.base_seed import BaseSeed
 from .models import UserFeedPosition
+from authentication.seeds import CustomUserSeed
+from stories.seeds import StorySeed
 
 
 class UserFeedPositionSeed(BaseSeed):
     raw_file = "user_feed_position"
     model = UserFeedPosition
+    dependencies = [
+        CustomUserSeed,
+        StorySeed,
+    ]
 
     @classmethod
     def get_fields(cls, item):
