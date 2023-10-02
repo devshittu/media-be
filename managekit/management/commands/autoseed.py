3c5a8fc5756e69461da8bd5b27e184fccd7594d7
Muhammed Shittu <devshittu@gmail.com>
2023-10-02 04:03:53 +0100

    Refactored to used dependency

--- managekit/management/commands/autoseed.py
+++ managekit/management/commands/autoseed.py
@@ -1,137 +1,127 @@
+import importlib
+import os
+from django.conf import settings
 from django.apps import apps
 from django.core.management import call_command
 from django.core.management.base import CommandError, BaseCommand
 from managekit.utils.base_seed import BaseSeed
-import importlib
-import os
-
-
-class ListOutput:
-    def __init__(self):
-        self.data = []
-
-    def write(self, s):
-        self.data.append(s)
-
-    def getvalue(self):
-        return "\n".join(self.data)
-
-
-class UnmigratedMigrationsError(CommandError):
-    pass
-
-# def check_for_unmigrated_migrations():
-#     # Capture the output of the showmigrations command
-#     output = ListOutput()
-#     call_command("showmigrations", stdout=output, stderr=output)
-#     output_str = output.getvalue()
-#     print(f"Processed data for {output_str}")
-
-#     # Check for any lines that don't start with "[X]", which indicates an unmigrated migration
-
-#     for line in output_str.splitlines():
-#         line = line.strip()  # Remove leading/trailing whitespace
-#         if (
-#             line
-#             and "_" in line
-#             and not line.startswith("[X]")
-#             and any(char.isdigit() for char in line)
-#         ):
-#             print(f"Unmigrated: {line}")  # Print the unmigrated line for debugging
-#             raise UnmigratedMigrationsError(
-#                 "There are unmigrated migrations. Please run 'python manage.py migrate' before using autoseed."
-#             )
 
 
 class Command(BaseCommand):
     help = "Automatically convert and seed data for all apps"
 
-    def handle(self, *args, **kwargs):
-        # Check for unmigrated migrations
-        # check_for_unmigrated_migrations()
-
-        retry_queue = []  # This will store BaseSeed subclasses that need to be retried
-
-        def process_seed(app, seed_class):
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f'Processing model: {seed_class.__name__.replace("Seed", "")}'
-                )
-            )
-
-            # Create an instance of the seed class
-            seed_instance = seed_class()
-
-            # Process the data
-            seed_instance.process_data(
-                app.name, seed_class.__name__.replace("Seed", "").lower(), app.path
-            )
-            self.stdout.write(
-                self.style.SUCCESS(
-                    f"Processed data for {app.name}.{seed_class.__name__.replace('Seed', '')}"
-                )
-            )
-
-            # Load the processed data into the database
-            try:
-                fixture_file = seed_instance.get_output_path(app.path)
-                call_command("loaddata", fixture_file)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Loaded data from {fixture_file} into the database"
-                    )
-                )
-                
-            except Exception as e:
-                # If there's an error, add the seed_class to the retry queue
-                retry_queue.append((app, seed_class))
-                self.stdout.write(
-                    self.style.WARNING(
-                        f"Error loading data for {app.name}.{seed_class.__name__}. Reason: {str(e)}. Will retry later."
-                    )
-                )
-
-
-        # Process all apps and their seeds
+    def build_dependency_graph(self):
+        graph = {}
         for app in apps.get_app_configs():
-            self.stdout.write(self.style.SUCCESS(f"Checking app: {app.name}"))
-            if os.path.exists(os.path.join(app.path, "data")):
-                self.stdout.write(
-                    self.style.SUCCESS(f"Found 'data' directory in {app.name}")
+            # Only process apps that are in the CUSTOM_APPS list
+            if app.name in settings.CUSTOM_APPS:
+                try:
+                    seed_module = importlib.import_module(f"{app.name}.seeds")
+                    for attr_name in dir(seed_module):
+                        attr = getattr(seed_module, attr_name)
+                        if (
+                            isinstance(attr, type)
+                            and issubclass(attr, BaseSeed)
+                            and attr is not BaseSeed
+                        ):
+                            graph[attr] = getattr(attr, "dependencies", [])
+                except ImportError:
+                    # If the seeds module doesn't exist, just continue to the next app
+                    continue
+        return graph
+
+    def topological_sort(self, graph):
+        visited = set()
+        visiting = set()  # Nodes that are currently being visited
+        order = []
+
+        def visit(node):
+            if node in visiting:
+                raise CommandError(f"Circular dependency detected: {node}")
+            if node not in visited:
+                visiting.add(node)
+                visited.add(node)
+                for neighbor in graph[node]:
+                    visit(neighbor)
+                visiting.remove(node)
+                order.append(node)
+
+        for node in graph:
+            if node not in visited:
+                visit(node)
+
+        return order
+
+    def process_seed(self, app, seed_class):
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Processing model: {seed_class.__name__.replace("Seed", "")}'
+            )
+        )
+
+        # Create an instance of the seed class
+        seed_instance = seed_class()
+
+        # Process the data
+        seed_instance.process_data(
+            app.name, seed_class.__name__.replace("Seed", "").lower(), app.path
+        )
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Processed data for {app.name}.{seed_class.__name__.replace('Seed', '')}"
+            )
+        )
+
+        # Load the processed data into the database
+        try:
+            fixture_file = seed_instance.get_output_path(app.path)
+            call_command("loaddata", fixture_file)
+            self.stdout.write(
+                self.style.SUCCESS(f"Loaded data from {fixture_file} into the database")
+            )
+        except Exception as e:
+            # If there's an error, add the seed_class to the retry queue
+            self.retry_queue.append((app, seed_class))
+            self.stdout.write(
+                self.style.WARNING(
+                    f"Error loading data for {app.name}.{seed_class.__name__}. Reason: {str(e)}. Will retry later."
                 )
-                seed_module = importlib.import_module(f"{app.name}.seeds")
-                for attr_name in dir(seed_module):
-                    attr = getattr(seed_module, attr_name)
-                    if (
-                        isinstance(attr, type)
-                        and issubclass(attr, BaseSeed)
-                        and attr is not BaseSeed
-                    ):
-                        process_seed(app, attr)
-            else:
-                self.stdout.write(
-                    self.style.WARNING(f"No 'data' directory found in {app.name}")
-                )
+            )
+
+    def handle(self, *args, **kwargs):
+        self.retry_queue = (
+            []
+        )  # This will store BaseSeed subclasses that need to be retried
+
+        graph = self.build_dependency_graph()
+        order = self.topological_sort(graph)
+
+        for SeedClass in order:
+            app = apps.get_app_config(SeedClass.model._meta.app_label)
+            self.process_seed(app, SeedClass)
 
         # Retry for seeds in the retry queue
         max_retries = 3  # You can adjust this number based on your needs
         for _ in range(max_retries):
-            if not retry_queue:
+            if not self.retry_queue:
                 break  # If the retry queue is empty, we're done
 
             self.stdout.write(
-                self.style.SUCCESS(f"Retrying for {len(retry_queue)} seeds...")
+                self.style.SUCCESS(f"Retrying for {len(self.retry_queue)} seeds...")
             )
-            seeds_to_retry = retry_queue.copy()
-            retry_queue.clear()  # Clear the retry queue for this iteration
+            seeds_to_retry = self.retry_queue.copy()
+            self.retry_queue.clear()  # Clear the retry queue for this iteration
 
             for app, seed_class in seeds_to_retry:
-                process_seed(app, seed_class)
+                self.process_seed(app, seed_class)
 
-        if retry_queue:
+        if self.retry_queue:
             # If there are still seeds left in the retry queue after all retries, log an error
             failed_seeds = ", ".join(
-                [f"{app.name}.{seed_class.__name__}" for app, seed_class in retry_queue]
+                [
+                    f"{app.name}.{seed_class.__name__}"
+                    for app, seed_class in self.retry_queue
+                ]
             )
             self.stdout.write(
                 self.style.ERROR(
@@ -143,4 +133,4 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS("Autoseed completed!"))
 
 
-# autoseed/management/commands/autoseed.py
+# managekit/management/commands/autoseed.py
