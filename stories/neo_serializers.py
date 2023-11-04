8af05856a5e3237b9fa5263e30e099c9e34955fd
Muhammed Shittu <devshittu@gmail.com>
2023-11-04 03:15:06 +0000

    Added get_hashtags to neo_serializers

--- stories/neo_serializers.py
+++ stories/neo_serializers.py
@@ -7,7 +7,7 @@ class StorylineSerializer(serializers.Serializer):
     description = serializers.CharField()
     summary = serializers.CharField()
     subject = serializers.CharField()
-    hashtags = serializers.CharField()
+    hashtags = serializers.SerializerMethodField()
     stories_count = serializers.IntegerField(read_only=True)
 
     class Meta:
@@ -19,7 +19,14 @@ class StorylineSerializer(serializers.Serializer):
             "subject",
             "hashtags",
             "stories_count",
+            "created_at",
+            "updated_at",
         ]
+    
+    def get_hashtags(self, obj):
+        # Assuming the get_hashtags method in the Storyline model returns a list of Hashtag objects
+        hashtags = obj.get_hashtags()
+        return [hashtag.name for hashtag in hashtags]
 
 
 class HashtagSerializer(serializers.Serializer):
