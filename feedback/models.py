3b9df07cd6e3fd5b63b3e1d25fd003dabd7e6c93
Muhammed Shittu <devshittu@gmail.com>
2023-11-04 03:05:54 +0000

    Updated Report model.

--- feedback/models.py
+++ feedback/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth import get_user_model
 from utils.models import SoftDeletableModel, TimestampedModel
 
@@ -14,9 +16,9 @@ class Report(SoftDeletableModel, TimestampedModel):
     Model to represent reports or flags raised by users.
     """
     user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True)
-    content_type = models.ForeignKey(models.ContentType, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    content_object = models.GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey('content_type', 'object_id')
     report_type = models.CharField(max_length=15, choices=REPORT_CHOICES)
     description = models.TextField(blank=True, null=True)
     is_anonymous = models.BooleanField(default=False)
@@ -34,3 +36,8 @@ class Report(SoftDeletableModel, TimestampedModel):
             # For example, you can set an 'is_flagged' attribute to True
             self.content_object.is_flagged = True
             self.content_object.save()
+
+    class Meta:
+        app_label = 'feedback'
+
+# feedback/models.py 
