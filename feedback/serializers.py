471ad140f041c7e5e7dbfa92a11d77446265d307
Muhammed Shittu <devshittu@gmail.com>
2023-11-04 03:06:48 +0000

    Updated GenericReportSerializer

--- feedback/serializers.py
+++ feedback/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
+from django.contrib.contenttypes.models import ContentType
 from .models import Report
+from utils.serializers import UnixTimestampModelSerializer
 
-class GenericReportSerializer(serializers.ModelSerializer):
-    """
-    Serializer for the Report model.
-    """
+class GenericReportSerializer(UnixTimestampModelSerializer):
+    content_type_name = serializers.CharField(write_only=True)
+
     class Meta:
         model = Report
         fields = '__all__'
+        extra_kwargs = {
+            'content_type': {'read_only': True}
+        }
+
+    def validate_content_type_name(self, value):
+        try:
+            return ContentType.objects.get(model=value.lower())
+        except ContentType.DoesNotExist:
+            raise serializers.ValidationError(f"No content type exists for model {value}")
