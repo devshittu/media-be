f37967bdab50c8769c7d124a7aa09cf05bde11e3
Muhammed Shittu <devshittu@gmail.com>
2023-11-04 03:07:19 +0000

    Updated ReportCreateView

--- feedback/views.py
+++ feedback/views.py
@@ -1,11 +1,8 @@
 from rest_framework import generics
-from .models import Report
-from .serializers import ReportSerializer
-from rest_framework import generics
+from django.contrib.contenttypes.models import ContentType
 from .models import Report
 from .serializers import GenericReportSerializer
 from utils.mixins import ReportCreationMixin
-
 
 
 class ReportCreateView(ReportCreationMixin, generics.CreateAPIView):
@@ -13,10 +10,12 @@ class ReportCreateView(ReportCreationMixin, generics.CreateAPIView):
     serializer_class = GenericReportSerializer
 
     def perform_create(self, serializer):
+        content_type = serializer.validated_data.pop('content_type_name', None)
+        
         if self.request.user.is_authenticated and not serializer.validated_data.get('is_anonymous'):
-            serializer.save(user=self.request.user)
+            serializer.save(user=self.request.user, content_type=content_type)
         else:
-            serializer.save()
+            serializer.save(content_type=content_type)
         
         # Handle post report creation
         instance = serializer.instance
