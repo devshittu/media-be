9492971bfec4b1e95cb5e7e43c8e48faa9c339fb
Muhammed Shittu <devshittu@gmail.com>
2023-11-08 02:55:12 +0000

    Disabled some validation.

--- analytics/serializers.py
+++ analytics/serializers.py
@@ -1,90 +1,110 @@
 from rest_framework import serializers
-from utils.serializers import UnixTimestampDateTimeField
+from utils.serializers import UnixTimestampModelSerializer
 from .models import (
-    StoryInteraction, StoryInteractionMetadataSchema, AccessibilityTool, UserNotInterested, UserSession
+    StoryInteraction,
+    StoryInteractionMetadataSchema,
+    AccessibilityTool,
+    UserNotInterested,
+    UserSession,
 )
 from jsonschema import validate, ValidationError
+from authentication.models import CustomUser
+
 
 class ViewMetadataSerializer(serializers.Serializer):
     source_page = serializers.CharField()
 
+
 class BookmarkMetadataSerializer(serializers.Serializer):
     source_page = serializers.CharField()
 
+
 class ShareMetadataSerializer(serializers.Serializer):
-    platform = serializers.ChoiceField(choices=['WhatsApp', 'Twitter', 'Facebook', 'Other'])
+    platform = serializers.ChoiceField(
+        choices=["WhatsApp", "Twitter", "Facebook", "Other"]
+    )
+
 
 class ClickExternalMetadataSerializer(serializers.Serializer):
     link_url = serializers.URLField()
+
 
 class ViewStorylineMetadataSerializer(serializers.Serializer):
     source_section = serializers.CharField()
 
 
-class AccessibilityToolSerializer(UnixTimestampDateTimeField):
+class AccessibilityToolSerializer(UnixTimestampModelSerializer):
     class Meta:
         model = AccessibilityTool
-        fields = '__all__'
+        fields = "__all__"
 
 
-
-class StoryInteractionSerializer(UnixTimestampDateTimeField):
+class StoryInteractionSerializer(UnixTimestampModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=CustomUser.objects.all(),
+        required=False,  # The user field is not required
+        allow_null=True,  # Allow null values
+    )
 
     class Meta:
         model = StoryInteraction
-        fields = ['id', 'user', 'story', 'interaction_type', 
-                  'created_at', 'metadata', 'device_data', 
-                  'location_data', 'referral_data']
-
+        fields = [
+            "id",
+            "user",
+            "story",
+            "interaction_type",
+            "created_at",
+            "metadata",
+        ]
 
     def validate(self, data):
-        interaction_type = data.get('interaction_type')
-        metadata = data.get('metadata', {})
-        version = metadata.get('version')
+        # interaction_type = data.get("interaction_type")
+        # metadata = data.get("metadata", {})
+        # version = metadata.get(
+        #     "version", "2.0"
+        # )  # Provide a default version if not specified
 
-        # Fetch the schema from the registry
-        schema_record = StoryInteractionMetadataSchema.objects.get(interaction_type=interaction_type, version=version)
-        schema = schema_record.schema
+        # # Fetch the schema from the registry
+        # try:
+        #     schema_record = StoryInteractionMetadataSchema.objects.get(
+        #         interaction_type=interaction_type, version=version
+        #     )
+        # except StoryInteractionMetadataSchema.DoesNotExist:
+        #     raise serializers.ValidationError(
+        #         f"Schema not found for interaction type '{interaction_type}' and version '{version}'."
+        #     )
 
-        # Validate metadata against the schema
-        try:
-            validate(metadata, schema)
-        except ValidationError as e:
-            raise serializers.ValidationError(f"Invalid metadata: {str(e)}")
+        # schema = schema_record.schema
+
+        # # Validate metadata against the schema
+        # try:
+        #     validate(metadata, schema)
+        # except ValidationError as e:
+        #     raise serializers.ValidationError(f"Invalid metadata: {str(e)}")
 
         return data
-        # # Map interaction types to their respective metadata serializers
-        # metadata_serializers = {
-        #     'view': ViewMetadataSerializer,
-        #     'bookmark': BookmarkMetadataSerializer,
-        #     'share': ShareMetadataSerializer,
-        #     'click_external': ClickExternalMetadataSerializer,
-        #     'view_storyline': ViewStorylineMetadataSerializer,
-        #     # ... add more mappings as needed ...
-        # }
 
-        # # Get the appropriate serializer for the interaction type
-        # metadata_serializer_class = metadata_serializers.get(interaction_type)
-
-        # if metadata_serializer_class:
-        #     metadata_serializer = metadata_serializer_class(data=metadata)
-        #     if not metadata_serializer.is_valid():
-        #         raise serializers.ValidationError({"metadata": metadata_serializer.errors})
-
-        # return data
-    
+    def create(self, validated_data):
+        # If the user is authenticated, add them to the validated_data
+        request = self.context.get("request")
+        if request and hasattr(request, "user") and request.user.is_authenticated:
+            validated_data["user"] = request.user
+        return super().create(validated_data)
 
 
-class UserNotInterestedSerializer(UnixTimestampDateTimeField):
+class UserNotInterestedSerializer(UnixTimestampModelSerializer):
     """
     Serializer for the UserNotInterested model.
     This serializer provides a representation of the data for stories users are not interested in.
     """
-    user = serializers.ReadOnlyField(source='user.username')  # Display the username of the user
+
+    user = serializers.ReadOnlyField(
+        source="user.username"
+    )  # Display the username of the user
 
     class Meta:
         model = UserNotInterested
-        fields = ['id', 'user', 'story', 'created_at', 'reason']
+        fields = ["id", "user", "story", "created_at", "reason"]
 
     # def validate(self, data):
     #     """
@@ -95,26 +115,39 @@ class UserNotInterestedSerializer(UnixTimestampDateTimeField):
     #     return data
 
 
-
-
-class UserSessionSerializer(UnixTimestampDateTimeField):
+class UserSessionSerializer(UnixTimestampModelSerializer):
     """
     Serializer for the UserSession model.
     This serializer provides a representation of a user's session, including associated device and location data.
     """
-    user = serializers.ReadOnlyField(source='user.username')  # Display the username of the user
+
+    user = serializers.ReadOnlyField(
+        source="user.username"
+    )  # Display the username of the user
 
     class Meta:
         model = UserSession
-        fields = ['id', 'user', 'session_id', 'device_data', 'location_data', 'start_timestamp'
-                  'device_type', 'operating_system', 'browser', 'screen_resolution', 'connection_type',
-                  'ip_address', 'geolocation', 'time_zone']
+        fields = [
+            "id",
+            "user",
+            "session_id",
+            "device_data",
+            "location_data",
+            "start_timestamp" "device_type",
+            "operating_system",
+            "browser",
+            "screen_resolution",
+            "connection_type",
+            "ip_address",
+            "geolocation",
+            "time_zone",
+        ]
 
     def create(self, validated_data):
         """
         Override the create method to handle nested serializers.
         """
-        user_session = UserSession.objects.create( **validated_data)
+        user_session = UserSession.objects.create(**validated_data)
         return user_session
 
 
