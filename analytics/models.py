5ffc8c007057c888623dfbd5d1295dce25ce66d7
Muhammed Shittu <devshittu@gmail.com>
2023-11-08 02:54:43 +0000

    modified some field to be nullable.

--- analytics/models.py
+++ analytics/models.py
@@ -3,17 +3,23 @@ from django.conf import settings
 from django.db.models import JSONField
 from utils.models import SoftDeletableModel, TimestampedModel
 
+
 class UserSession(SoftDeletableModel, TimestampedModel):
     """
     Model to represent a user's session.
     """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_sessions')
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user_sessions"
+    )
     session_token = models.UUIDField(unique=True)  # Unique identifier for the session
-    # location_data 
+    # location_data
     ip_address = models.GenericIPAddressField(null=True, blank=True)
-    geolocation = models.CharField(max_length=255, null=True, blank=True)  # This could be further normalized if needed
+    geolocation = models.CharField(
+        max_length=255, null=True, blank=True
+    )  # This could be further normalized if needed
     time_zone = models.CharField(max_length=50, null=True, blank=True)
-    # device_data 
+    # device_data
     device_type = models.CharField(max_length=50, null=True, blank=True)
     operating_system = models.CharField(max_length=50, null=True, blank=True)
     browser = models.CharField(max_length=50, null=True, blank=True)
@@ -23,8 +29,8 @@ class UserSession(SoftDeletableModel, TimestampedModel):
     start_timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        verbose_name = 'User Session'
-        verbose_name_plural = 'User Sessions'
+        verbose_name = "User Session"
+        verbose_name_plural = "User Sessions"
 
     def __str__(self):
         return f"Session {self.session_id} for {self.user}"
@@ -34,53 +40,76 @@ class StoryInteraction(SoftDeletableModel, TimestampedModel):
     """
     Generic model to track various interactions with stories.
     """
+
     INTERACTION_CHOICES = [
-        ('view', 'View'),
-        ('bookmark', 'Bookmark'),
-        ('unbookmark', 'Unbookmark'),
-        ('share', 'Share'),
-        ('click_external', 'Click External Link'),
-        ('view_storyline', 'View Storyline'),
-        ('report', 'Report story'),
-        ('highlight_text', 'Highlight Text'),
+        ("view", "View"),
+        ("bookmark", "Bookmark"),
+        ("unbookmark", "Unbookmark"),
+        ("share", "Share"),
+        ("click_external", "Click External Link"),
+        ("view_storyline", "View Storyline"),
+        ("report", "Report story"),
+        ("highlight_text", "Highlight Text"),
         # ... other interactions ...
     ]
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='story_interactions')
-    story = models.ForeignKey('stories.Story', on_delete=models.CASCADE, related_name='interactions')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="story_interactions",
+        null=True,
+        blank=True,
+    )
+    story = models.ForeignKey(
+        "stories.Story", on_delete=models.CASCADE, related_name="interactions"
+    )
     interaction_type = models.CharField(max_length=50, choices=INTERACTION_CHOICES)
     metadata = JSONField(blank=True, null=True)  # Additional data for the interaction
-    user_session = models.ForeignKey(UserSession, on_delete=models.CASCADE, related_name='interactions')
+    user_session = models.ForeignKey(
+        UserSession, on_delete=models.CASCADE, related_name="interactions", null=True, blank=True
+    )
 
     class Meta:
-        verbose_name = 'Story Interaction'
-        verbose_name_plural = 'Stories Interactions'
+        verbose_name = "Story Interaction"
+        verbose_name_plural = "Stories Interactions"
 
     def __str__(self):
         return f"{self.user} {self.interaction_type} {self.story} on {self.created_at}"
 
 
 class StoryInteractionMetadataSchema(SoftDeletableModel, TimestampedModel):
-    interaction_type = models.CharField(max_length=50, choices=StoryInteraction.INTERACTION_CHOICES)
+    interaction_type = models.CharField(
+        max_length=50, choices=StoryInteraction.INTERACTION_CHOICES
+    )
     version = models.CharField(max_length=10)
     schema = JSONField()
 
     class Meta:
-        unique_together = ('interaction_type', 'version')
+        unique_together = ("interaction_type", "version")
+
+    def __str__(self):
+        return f"{self.interaction_type} v{self.version}"
 
 
 class UserNotInterested(SoftDeletableModel, TimestampedModel):
     """
     Model to track stories users are not interested in.
     """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='uninterested_stories')
-    story = models.ForeignKey('stories.Story', on_delete=models.CASCADE)
-    reason = models.TextField(blank=True, null=True)  # Optional field if you want to capture why they're not interested
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="uninterested_stories",
+    )
+    story = models.ForeignKey("stories.Story", on_delete=models.CASCADE)
+    reason = models.TextField(
+        blank=True, null=True
+    )  # Optional field if you want to capture why they're not interested
 
     class Meta:
-        verbose_name = 'User Not Interested'
-        verbose_name_plural = 'Users Not Interested'
-        ordering = ['-created_at']
+        verbose_name = "User Not Interested"
+        verbose_name_plural = "Users Not Interested"
+        ordering = ["-created_at"]
 
     def __str__(self):
         return f"{self.user} not interested in {self.story} on {self.created_at}"
@@ -90,20 +119,26 @@ class AccessibilityTool(SoftDeletableModel, TimestampedModel):
     """
     Model to track accessibility tools used.
     """
+
     TOOL_CHOICES = [
-        ('screen_reader', 'Screen Reader'),
-        ('magnifier', 'Magnifier'),
+        ("screen_reader", "Screen Reader"),
+        ("magnifier", "Magnifier"),
         # ... other tools ...
     ]
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='accessibility_tools')
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="accessibility_tools",
+    )
     tool_name = models.CharField(max_length=100, choices=TOOL_CHOICES)
 
     class Meta:
-        verbose_name = 'Accessibility Tool'
-        verbose_name_plural = 'Accessibility Tools'
+        verbose_name = "Accessibility Tool"
+        verbose_name_plural = "Accessibility Tools"
 
     def __str__(self):
         return f"{self.user} used {self.tool_name} on {self.created_at}"
+
 
 # analytics/models.py
