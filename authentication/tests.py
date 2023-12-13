dea98ae185ed5eae3bce00c091cb5ae634c13ea1
Muhammed Shittu <devshittu@gmail.com>
2023-12-13 23:43:19 +0000

    Added and updated display_name to replace .name

--- authentication/tests.py
+++ authentication/tests.py
@@ -3,15 +3,15 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from .models import CustomUser
 
-class RegistrationTestCase(APITestCase):
 
+class RegistrationTestCase(APITestCase):
     def test_registration(self):
         data = {
             "email": "testuser@test.com",
             "username": "testuser",
             "password": "strong_password_123",
-            "name": "Test User"
+            "display_name": "Test User",
         }
-        response = self.client.post(reverse('name-of-registration-view'), data)
+        response = self.client.post(reverse("name-of-registration-view"), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(CustomUser.objects.count(), 1)
