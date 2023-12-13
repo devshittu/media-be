65b2b49d3914f0490147a71128d89cf66002ac3e
Muhammed Shittu <devshittu@gmail.com>
2023-12-13 23:41:57 +0000

    Added UpdatePasswordSerializer and UpdateUserSerializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,29 +1,32 @@
 from .models import CustomUser, VerificationToken
 from rest_framework import serializers
+from django.contrib.auth import password_validation
+from django.contrib.auth.hashers import make_password
+
 # from common.serializers import CustomUserSerializer
+
 
 class CustomUserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['email', 'name',  'username', 'password']
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
+        fields = ["email", "display_name", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = CustomUser(
-            email=validated_data['email'],
-            name=validated_data['name'],
-            username=validated_data.get('username', None) or f"user_{validated_data['email'].split('@')[0]}"
+            email=validated_data["email"],
+            display_name=validated_data["display_name"],
+            username=validated_data.get("username", None)
+            or f"user_{validated_data['email'].split('@')[0]}",
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data["password"])
         user.save()
         return user
 
 
-
 class PasswordResetRequestSerializer(serializers.Serializer):
     email = serializers.EmailField()
+
 
 class PasswordResetConfirmSerializer(serializers.Serializer):
     token = serializers.CharField()
@@ -33,7 +36,53 @@ class PasswordResetConfirmSerializer(serializers.Serializer):
 class VerificationTokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = VerificationToken
-        fields = ('token',)
+        fields = ("token",)
+
+
+class UpdatePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    confirm_new_password = serializers.CharField(required=True)
+
+    def validate_old_password(self, value):
+        user = self.context["request"].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Old password is not correct")
+        return value
+
+    def validate(self, data):
+        if data["new_password"] != data["confirm_new_password"]:
+            raise serializers.ValidationError("New passwords must match")
+        password_validation.validate_password(
+            data["new_password"], self.context["request"].user
+        )
+        return data
+
+    def save(self, **kwargs):
+        user = self.context["request"].user
+        user.password = make_password(self.validated_data["new_password"])
+        user.save()
+        return user
+
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "email", "display_name"]
+
+    def validate_email(self, value):
+        if CustomUser.objects.exclude(pk=self.instance.pk).filter(email=value).exists():
+            raise serializers.ValidationError("This email is already in use.")
+        return value
+
+    def validate_username(self, value):
+        if (
+            CustomUser.objects.exclude(pk=self.instance.pk)
+            .filter(username=value)
+            .exists()
+        ):
+            raise serializers.ValidationError("This username is already in use.")
+        return value
 
 
 # authentication/serializers.py
