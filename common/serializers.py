fc65e03dcca920c61b7e487ca24184cba698dfc2
Muhammed Shittu <devshittu@gmail.com>
2023-12-13 23:53:22 +0000

    Formatted

--- common/serializers.py
+++ common/serializers.py
@@ -3,17 +3,23 @@ from rest_framework import serializers
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = get_user_model()
         fields = [
-            'id', 'name', 'email', 'username', 'avatar_url', 'display_picture',
-            # 'date_of_birth', 
-            'bio', 
-            'phone_number', 
-            'last_activity', 
-            'roles',
-            'is_active', 'is_staff', 'has_completed_setup',
+            "id",
+            "display_name",
+            "email",
+            "username",
+            "avatar_url",
+            "display_picture",
+            # 'date_of_birth',
+            "bio",
+            "phone_number",
+            "last_activity",
+            "roles",
+            "is_active",
+            "is_staff",
+            "has_completed_setup",
         ]
 
 
@@ -24,24 +30,34 @@ class AuthUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = [
-            'id', 'name', 'email', 'username', 'avatar_url', 'display_picture',
-            # 'date_of_birth', 
-            'bio', 
-            'phone_number', 
-            'last_activity', 
-            'roles',
-            'is_active', 'is_staff', 'has_completed_setup',
-            'settings', 'feed_position'
+            "id",
+            "display_name",
+            "email",
+            "username",
+            "avatar_url",
+            "display_picture",
+            # 'date_of_birth',
+            "bio",
+            "phone_number",
+            "last_activity",
+            "roles",
+            "is_active",
+            "is_staff",
+            "has_completed_setup",
+            "settings",
+            "feed_position",
         ]
 
     def get_settings(self, obj):
         # Lazy import inside the method
         from users.serializers import UserSettingSerializer
+
         return UserSettingSerializer(obj.settings).data
 
     def get_feed_position(self, obj):
         # Lazy import inside the method
         from users.serializers import UserFeedPositionSerializer
+
         return UserFeedPositionSerializer(obj.userfeedposition).data
 
 
