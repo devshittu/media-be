acf7fc52c872cdea3e3857995480cafccc4b9a1c
Muhammed Shittu <devshittu@gmail.com>
2023-12-14 00:17:26 +0000

    Added update-password and update-user

--- users/urls.py
+++ users/urls.py
@@ -1,23 +1,55 @@
 from django.urls import path
-from .views import (UserListCreateView, UserRetrieveUpdateDestroyView, UserSettingListCreateView, 
-                    FollowUserView, UnfollowUserView, UserFollowersListView, 
-                    UpdateFeedPositionView, UnfollowedUsersView)
+from .views import (
+    UserListCreateView,
+    UserRetrieveUpdateDestroyView,
+    UserSettingListCreateView,
+    FollowUserView,
+    UnfollowUserView,
+    UserFollowersListView,
+    UpdateFeedPositionView,
+    UnfollowedUsersView,
+)
+from authentication.views import UpdatePasswordView, UpdateUserView
 
 urlpatterns = [
-    path('', UserListCreateView.as_view(), name='user-list-create'),
-    path('<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-retrieve-update-destroy'),
-
-    path('unfollowed/', UnfollowedUsersView.as_view(), name='unfollowed-users'),
-    
-    path('settings/', UserSettingListCreateView.as_view(), name='user-settings-list-create'),
-
-    path('update-feed-position/', UpdateFeedPositionView.as_view(), name='update-feed-position'),
-
-    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user-by-id'),
-    path('follow/<str:username>/', FollowUserView.as_view(), name='follow-user-by-username'),
-    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user-by-id'),
-    path('unfollow/<str:username>/', UnfollowUserView.as_view(), name='unfollow-user-by-username'),
-    
-    path('<int:user_id>/followers/', UserFollowersListView.as_view(), name='user-followers-list'),
-    
+    path("", UserListCreateView.as_view(), name="user-list-create"),
+    path(
+        "<int:pk>/",
+        UserRetrieveUpdateDestroyView.as_view(),
+        name="user-retrieve-update-destroy",
+    ),
+    path("unfollowed/", UnfollowedUsersView.as_view(), name="unfollowed-users"),
+    path(
+        "settings/",
+        UserSettingListCreateView.as_view(),
+        name="user-settings-list-create",
+    ),
+    path(
+        "update-feed-position/",
+        UpdateFeedPositionView.as_view(),
+        name="update-feed-position",
+    ),
+    path("update-password/", UpdatePasswordView.as_view(), name="update-password"),
+    path("update-user/", UpdateUserView.as_view(), name="update-user"),
+    path("follow/<int:user_id>/", FollowUserView.as_view(), name="follow-user-by-id"),
+    path(
+        "follow/<str:username>/",
+        FollowUserView.as_view(),
+        name="follow-user-by-username",
+    ),
+    path(
+        "unfollow/<int:user_id>/",
+        UnfollowUserView.as_view(),
+        name="unfollow-user-by-id",
+    ),
+    path(
+        "unfollow/<str:username>/",
+        UnfollowUserView.as_view(),
+        name="unfollow-user-by-username",
+    ),
+    path(
+        "<int:user_id>/followers/",
+        UserFollowersListView.as_view(),
+        name="user-followers-list",
+    ),
 ]
