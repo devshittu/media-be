502216baac8948c6cd74d5b83ae24b3a296b29e8
Muhammed Shittu <devshittu@gmail.com>
2023-12-20 23:41:59 +0000

    Added fields explicitly for multimedia serializers

--- multimedia/serializers.py
+++ multimedia/serializers.py
@@ -1,14 +1,27 @@
 from rest_framework import serializers
 from .models import Multimedia
 
+
 class MultimediaSerializer(serializers.ModelSerializer):
     """
     Serializer for the Multimedia model.
     """
-    user = serializers.ReadOnlyField(source='user.email')  # Display the user's email
+
+    # user = serializers.ReadOnlyField(source="user.email")  # Display the user's email
+    # user = serializers. (source="user.email", write_only=True)  # Display the user's email
 
     class Meta:
         model = Multimedia
-        fields = '__all__'
+        fields = [
+            "file",
+            "media_url",
+            "caption",
+            "thumbnail",
+            "media_type",
+            "id",
+            "user",
+            "story",
+        ]
+
 
 # multimedia/serializers.py
