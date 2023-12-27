7b257a57a12d8c54c77b5d349dcd117a98edd37e
Muhammed Shittu <devshittu@gmail.com>
2023-12-27 22:12:02 +0000

    Added IsOwner

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,13 +1,21 @@
 from rest_framework import permissions
 
+
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
+
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed for any request
         if request.method in permissions.SAFE_METHODS:
             return True
 
         # Write permissions are only allowed to the owner of the object
+        return obj.user == request.user
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Object-level permission to only allow owners of an object to edit it
         return obj.user == request.user
