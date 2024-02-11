d12717b329b70e6ea2171f7b38e4f66448350da3
Muhammed Shittu <devshittu@gmail.com>
2024-02-11 22:15:43 +0000

    Formatted

--- core/celery.py
+++ core/celery.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from decouple import config
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 # app = Celery('media-be')
-app = Celery(config('APP_CODE_NAME', default='appname-media-be', cast=str))
+app = Celery(config("APP_CODE_NAME", default="appname-media-be", cast=str))
 
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+# core/celery.py
