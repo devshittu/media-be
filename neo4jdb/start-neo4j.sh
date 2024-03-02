222ea3700b750c69a4ef4ddb572b6b9683f7eaa9
Muhammed Shittu <devshittu@gmail.com>
2024-03-02 23:54:13 +0000

    Removed unneeded script

--- neo4jdb/start-neo4j.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Assuming NEOMODEL_NEO4J_AUTH_USERNAME and NEOMODEL_NEO4J_AUTH_PASSWORD are passed as environment variables
-export NEO4J_AUTH="${NEOMODEL_NEO4J_AUTH_USERNAME}:${NEOMODEL_NEO4J_AUTH_PASSWORD}"
-
-# Now, run the main container process
-exec "$@"
-
-# neo4jdb/start-neo4j.sh
