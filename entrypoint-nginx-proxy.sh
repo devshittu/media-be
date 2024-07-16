a37be79d4a489c71e4fe104d6d26d1f8a11130b6
Muhammed Shittu <devshittu@gmail.com>
2024-07-16 17:36:37 +0100

    Fix SSL configuration and ensure directory creation for Nginx proxy
    - Remove manual SSL configuration from nginx.staging.conf.
    - Ensure required directories are created in entrypoint script.
    - Update docker-compose file to use correct volumes and entrypoint script.

--- entrypoint-nginx-proxy.sh
+++ entrypoint-nginx-proxy.sh
@@ -3,6 +3,8 @@
 # Create required directories
 mkdir -p /etc/nginx/conf.d
 mkdir -p /usr/share/nginx/html/.well-known/acme-challenge
+mkdir -p /etc/nginx/vhost.d
+mkdir -p /etc/nginx/certs
 
 # Execute the original command
 exec "$@"
