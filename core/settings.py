39da2be8d7442f216e2a134a7ab628c895ce1f14
Muhammed Shittu <devshittu@gmail.com>
2024-07-26 10:26:58 +0100

    feat: Integrate custom Nginx image with CI/CD pipeline
    - Modify Dockerfile for Nginx proxy to copy static.conf
    - Update ci-docker-hub.yml to build and push custom Nginx image
    - Update cd-vm-deploy.yml to pull and deploy custom Nginx image
    - Modify docker-compose.staging.yml to use custom Nginx image
    - Ensure correct volume mounts and environment variables in CD pipeline

--- core/settings.py
+++ core/settings.py
@@ -264,7 +264,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-MEDIA_URL = "/mediafiles/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 # TODO: this works without the minio
 
