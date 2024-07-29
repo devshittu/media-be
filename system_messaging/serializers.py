6013f5ef01e0f9dc60b40346ac170b2721cc3be2
Muhammed Shittu <devshittu@gmail.com>
2024-07-29 14:10:50 +0100

    Formatted

--- system_messaging/serializers.py
+++ system_messaging/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import MessageTemplate
 
+
 class MessageTemplateSerializer(serializers.ModelSerializer):
     class Meta:
         model = MessageTemplate
         fields = '__all__'
+
+# system_messaging/serializers.py
