cebea4d9a81e48e504da4df904fca8bce9c525c8
Muhammed Shittu <devshittu@gmail.com>
2024-07-29 14:41:38 +0100

    feat(logging): Add logging to MultimediaAdmin in Django admin
    - Added DEBUG logs for tracing the saving and deleting of multimedia objects in the admin interface.
    - Added INFO logs for successful save and delete operations by specific users.

--- multimedia/admin.py
+++ multimedia/admin.py
@@ -1,5 +1,12 @@
+# multimedia/admin.py
+
+import logging
 from django.contrib import admin
 from .models import Multimedia
+
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
+
 
 class MultimediaAdmin(admin.ModelAdmin):
     list_display = ('media_type', 'user', 'story', 'created_at', 'updated_at')
@@ -7,4 +14,18 @@ class MultimediaAdmin(admin.ModelAdmin):
     search_fields = ('media_type', 'user__email', 'story__id')
     ordering = ('-created_at',)
 
+    def save_model(self, request, obj, form, change):
+        logger.debug(f'Saving multimedia object {obj.id}')
+        super().save_model(request, obj, form, change)
+        logger.info(
+            f'Multimedia object {obj.id} saved by {request.user.email}')
+
+    def delete_model(self, request, obj):
+        logger.debug(f'Deleting multimedia object {obj.id}')
+        super().delete_model(request, obj)
+        logger.info(
+            f'Multimedia object {obj.id} deleted by {request.user.email}')
+
+
 admin.site.register(Multimedia, MultimediaAdmin)
+# multimedia/admin.py
