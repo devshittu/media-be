51df0e42d1d667bc137d27d73abf54cb9339a487
Muhammed Shittu <devshittu@gmail.com>
2024-07-30 02:54:58 +0100

    feat(logging): Add logging to MultimediaSerializer methods

--- multimedia/serializers.py
+++ multimedia/serializers.py
@@ -1,5 +1,9 @@
+import logging
 from rest_framework import serializers
 from .models import Multimedia
+
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
 
 
 class MultimediaSerializer(serializers.ModelSerializer):
@@ -23,5 +27,45 @@ class MultimediaSerializer(serializers.ModelSerializer):
             "story",
         ]
 
+    def to_representation(self, instance):
+        """
+        Convert `Multimedia` instance to a dictionary representation.
+        """
+        logger.debug(f'Serializing multimedia instance {instance.id}')
+        representation = super().to_representation(instance)
+        logger.info(
+            f'Multimedia instance {instance.id} serialized successfully')
+        return representation
+
+    def validate(self, attrs):
+        """
+        Validate the incoming data for the `Multimedia` instance.
+        """
+        logger.debug(f'Validating multimedia data: {attrs}')
+        validated_data = super().validate(attrs)
+        logger.info(f'Multimedia data validated successfully: {attrs}')
+        return validated_data
+
+    def create(self, validated_data):
+        """
+        Create a new `Multimedia` instance.
+        """
+        logger.debug(
+            f'Creating multimedia instance with data: {validated_data}')
+        multimedia_instance = super().create(validated_data)
+        logger.info(
+            f'Multimedia instance {multimedia_instance.id} created successfully')
+        return multimedia_instance
+
+    def update(self, instance, validated_data):
+        """
+        Update an existing `Multimedia` instance.
+        """
+        logger.debug(
+            f'Updating multimedia instance {instance.id} with data: {validated_data}')
+        multimedia_instance = super().update(instance, validated_data)
+        logger.info(
+            f'Multimedia instance {multimedia_instance.id} updated successfully')
+        return multimedia_instance
 
 # multimedia/serializers.py
