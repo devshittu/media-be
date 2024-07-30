295a87fa6bc32a2954955f29e34d825da3459f99
Muhammed Shittu <devshittu@gmail.com>
2024-07-30 01:38:57 +0100

    feat(logging): Add logging to test_sendgrid_email management command

--- managekit/management/commands/test_sendgrid_email.py
+++ managekit/management/commands/test_sendgrid_email.py
@@ -1,5 +1,9 @@
+import logging
 from django.core.management.base import BaseCommand
 from system_messaging.tasks import send_test_email
+
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
 
 
 class Command(BaseCommand):
@@ -11,9 +15,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         recipient = kwargs['recipient']
+        logger.info(f'Triggering test email to {recipient}')
         send_test_email.delay(recipient)
         self.stdout.write(self.style.SUCCESS(
             f'Triggered test email to {recipient}'))
+        logger.info(f'Successfully triggered test email to {recipient}')
 
 # python manage.py test_sendgrid_email devshittu@gmail.com
 # managekit/management/commands/test_sendgrid_email.py
