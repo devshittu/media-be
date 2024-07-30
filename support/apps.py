7d97287e5964c8523c78f68cc5b1af399545ff1f
Muhammed Shittu <devshittu@gmail.com>
2024-07-30 02:14:55 +0100

    feat(logging): Add logging to SupportConfig ready method

--- support/apps.py
+++ support/apps.py
@@ -1,6 +1,15 @@
+import logging
 from django.apps import AppConfig
+
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
 
 
 class SupportConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'support'
+
+    def ready(self):
+        logger.debug('Support app is ready')
+
+# support/apps.py
