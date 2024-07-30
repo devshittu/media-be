2d65edf5a643ab814b5cfa91fc1010edc96d0bf6
Muhammed Shittu <devshittu@gmail.com>
2024-07-30 04:00:29 +0100

    feat(logging): Add logging to serializers in neo_serializers

--- stories/neo_serializers.py
+++ stories/neo_serializers.py
@@ -1,8 +1,13 @@
+import logging
 from rest_framework import serializers
 from .neo_models import Storyline
 
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
+
 
 class StorylineSerializer(serializers.Serializer):
+    logger.debug('Initializing StorylineSerializer')
     id = serializers.CharField()
     description = serializers.CharField()
     summary = serializers.CharField()
@@ -22,14 +27,22 @@ class StorylineSerializer(serializers.Serializer):
             "created_at",
             "updated_at",
         ]
-    
+
     def get_hashtags(self, obj):
+        logger.debug(f'Getting hashtags for storyline {obj.id}')
         # Assuming the get_hashtags method in the Storyline model returns a list of Hashtag objects
-        hashtags = obj.get_hashtags()
-        return [hashtag.name for hashtag in hashtags]
+        try:
+            hashtags = obj.get_hashtags()
+            logger.info(f'Hashtags retrieved for storyline {obj.id}')
+            return [hashtag.name for hashtag in hashtags]
+        except Exception as e:
+            logger.error(
+                f'Error retrieving hashtags for storyline {obj.id}: {e}')
+            return []
 
 
 class HashtagSerializer(serializers.Serializer):
+    logger.debug('Initializing HashtagSerializer')
     name = serializers.CharField()
     stories_count = serializers.IntegerField()
 
