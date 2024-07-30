c20dc99fb82c59cc9cecbb37fc4805a8f65bc5a5
Muhammed Shittu <devshittu@gmail.com>
2024-07-30 03:54:47 +0100

    feat(logging): Add logging to models in stories

--- stories/models.py
+++ stories/models.py
@@ -1,3 +1,4 @@
+import logging
 from django.db import models
 from django.urls import reverse
 from django.conf import settings
@@ -5,17 +6,23 @@ from autoslug import AutoSlugField
 from .managers import StoryManager
 from utils.managers import SoftDeleteManager, ActiveUnflaggedManager
 from feedback.managers import ReportManager
-from utils.models import SoftDeletableModel, TimestampedModel, FlaggedContentMixin
+from utils.models import (
+    SoftDeletableModel, TimestampedModel, FlaggedContentMixin,)
+
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
 
 
 class Category(SoftDeletableModel, TimestampedModel):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    slug = AutoSlugField(populate_from="title", unique=True, always_update=True)
+    slug = AutoSlugField(populate_from="title",
+                         unique=True, always_update=True)
 
     objects = SoftDeleteManager()
 
     def __str__(self):
+        logger.debug(f'Returning string representation for category {self.id}')
         return self.title
 
     class Meta:
@@ -25,6 +32,7 @@ class Category(SoftDeletableModel, TimestampedModel):
 
 class Story(FlaggedContentMixin, SoftDeletableModel, TimestampedModel):
     """Model representing a user's story."""
+    logger.debug('Initializing Story model')
 
     title = models.CharField(max_length=100)
     slug = AutoSlugField(
@@ -78,14 +86,17 @@ class Story(FlaggedContentMixin, SoftDeletableModel, TimestampedModel):
 
     @property
     def likes_count(self):
+        logger.debug(f'Calculating likes count for story {self.id}')
         return self.likes_set.count()
 
     @property
     def dislikes_count(self):
+        logger.debug(f'Calculating dislikes count for story {self.id}')
         return self.dislikes_set.count()
 
     @property
     def trending_score(self):
+        logger.debug(f'Calculating trending score for story {self.id}')
         likes = self.likes_count
         dislikes = self.dislikes_count
         views = self.interactions.filter(interaction_type="view").count()
@@ -93,27 +104,34 @@ class Story(FlaggedContentMixin, SoftDeletableModel, TimestampedModel):
         return likes - dislikes + views
 
     def __str__(self):
+        logger.debug(f'Returning string representation for story {self.id}')
         return self.title
 
     class Meta:
         verbose_name_plural = "Stories"
 
     def get_absolute_url(self):
+        logger.debug(f'Getting absolute URL for story {self.id}')
         return reverse(
             "story-retrieve-update-destroy", kwargs={"story_slug": self.slug}
         )
 
 
 class Like(SoftDeletableModel, TimestampedModel):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    story = models.ForeignKey(Story, on_delete=models.CASCADE, related_name="likes_set")
+    logger.debug('Initializing Like model')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    story = models.ForeignKey(
+        Story, on_delete=models.CASCADE, related_name="likes_set")
 
     class Meta:
         unique_together = ["user", "story"]
 
 
 class Dislike(SoftDeletableModel, TimestampedModel):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    logger.debug('Initializing Dislike model')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     story = models.ForeignKey(
         Story, on_delete=models.CASCADE, related_name="dislikes_set"
     )
@@ -125,14 +143,17 @@ class Dislike(SoftDeletableModel, TimestampedModel):
 
 
 class Bookmark(SoftDeletableModel, TimestampedModel):
+    logger.debug('Initializing Bookmark model')
     BOOKMARK_CATEGORIES = [
         ("Read Later", "Read Later"),
         ("Favorites", "Favorites"),
         ("Save", "Save"),
     ]
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    story = models.ForeignKey(Story, on_delete=models.CASCADE, related_name="bookmarks")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    story = models.ForeignKey(
+        Story, on_delete=models.CASCADE, related_name="bookmarks")
     bookmark_category = models.CharField(
         max_length=50, choices=BOOKMARK_CATEGORIES, default="Read Later"
     )
@@ -140,29 +161,33 @@ class Bookmark(SoftDeletableModel, TimestampedModel):
 
     @property
     def title(self):
+        logger.debug(f'Getting title for bookmark {self.id}')
         return self.story.title
 
     @property
     def url(self):
+        logger.debug(f'Getting URL for bookmark {self.id}')
         # Assuming you have a get_absolute_url method in the Story model
         return self.story.get_absolute_url()
 
     @property
     def thumbnail_url(self):
+        logger.debug(f'Getting thumbnail URL for bookmark {self.id}')
         # Assuming the multimedia model has a method to get the thumbnail URL
         multimedia = self.story.multimedia.first()
         return multimedia.thumbnail.url if multimedia else None
 
     @property
     def story_published_at(self):
+        logger.debug(f'Getting story published date for bookmark {self.id}')
         return self.story.created_at
 
     class Meta:
-        # db_table = ''
         verbose_name = "Bookmark"
         verbose_name_plural = "Bookmarks"
 
     def __str__(self):
+        logger.debug(f'Returning string representation for bookmark {self.id}')
         return f"Bookmark of story: {self.story.title}"
 
 
