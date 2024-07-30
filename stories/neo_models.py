4b5811b1b594b0de4f26396ddeb3c58a62d98b77
Muhammed Shittu <devshittu@gmail.com>
2024-07-30 03:58:01 +0100

    feat(logging): Add logging to neo_models in stories

--- stories/neo_models.py
+++ stories/neo_models.py
@@ -1,9 +1,15 @@
+import logging
 from django_neomodel import DjangoNode
-from neomodel import (UniqueIdProperty, DateTimeProperty, 
-                      IntegerProperty, StringProperty,  
+from neomodel import (UniqueIdProperty, DateTimeProperty,
+                      IntegerProperty, StringProperty,
                       RelationshipTo, RelationshipFrom)
 
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
+
+
 class StoryNode(DjangoNode):
+    logger.debug('Initializing StoryNode model')
     node_id = UniqueIdProperty()
     story_id = IntegerProperty(unique_index=True)
     event_occurred_at = DateTimeProperty()
@@ -12,7 +18,9 @@ class StoryNode(DjangoNode):
     previous_story = RelationshipFrom('StoryNode', 'FOLLOWS')
     hashtags = RelationshipTo('Hashtag', 'HAS_HASHTAG')
 
+
 class Storyline(DjangoNode):
+    logger.debug('Initializing Storyline model')
     id = UniqueIdProperty()
     description = StringProperty()
     summary = StringProperty()
@@ -26,30 +34,43 @@ class Storyline(DjangoNode):
 
     @property
     def stories_count(self):
-        return len(self.stories.all())
-    
+        count = len(self.stories.all())
+        logger.debug(f'Calculating stories count: {count}')
+        return count
 
     def get_hashtags(self):
+        logger.debug(f'Retrieving hashtags for storyline {self.id}')
         # Get all stories associated with this storyline
         stories = self.stories.all()
-        
+
         # Collect all unique hashtag names from these stories
         hashtag_names = set()
         for story in stories:
             for hashtag in story.hashtags.all():
+                logger.debug(
+                    f'Adding hashtag {hashtag.name} from story {story.story_id}')
                 hashtag_names.add(hashtag.name)
-        
+
         # Retrieve the actual Hashtag objects using their names
-        hashtags = [Hashtag.nodes.get(name=hashtag_name) for hashtag_name in hashtag_names]
-        
+        hashtags = [Hashtag.nodes.get(name=hashtag_name)
+                    for hashtag_name in hashtag_names]
+
+        logger.info(
+            f'Hashtags retrieved for storyline {self.id}: {hashtag_names}')
         return hashtags
-    
+
+
 class Hashtag(DjangoNode):
+    logger.debug('Initializing Hashtag model')
+
     name = StringProperty(unique_index=True)
     stories = RelationshipFrom('StoryNode', 'HAS_HASHTAG')
 
     @property
     def stories_count(self):
-        return len(self.stories.all())
+        count = len(self.stories.all())
+        logger.debug(
+            f'Calculating stories count for hashtag {self.name}: {count}')
+        return count
 
 # stories/neo_models.py
