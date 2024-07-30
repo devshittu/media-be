2653bf5c31c2005e95f56500c513687ceb00a720
Muhammed Shittu <devshittu@gmail.com>
2024-07-30 04:55:55 +0100

    feat(logging): Add logging to views in stories

--- stories/views.py
+++ stories/views.py
@@ -1,26 +1,25 @@
-from django.db.models import Count
+import logging
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, filters, status, serializers
 from django.db import IntegrityError
 from users.models import UserSetting
 from stories.neo_models import StoryNode
 from .models import Story, Like, Dislike, Bookmark, Category
-from .serializers import StorySerializer, BookmarkSerializer, CategorySerializer
 from utils.permissions import CustomIsAuthenticated
 from utils.mixins import SoftDeleteMixin
 from .serializers import (
     LikeSerializer,
     DislikeSerializer,
     CategorySerializer,
-    TrendingStorySerializer,
+    TrendingStorySerializer, StorySerializer, BookmarkSerializer
 )
 from utils.pagination import CustomPageNumberPagination
 from utils.permissions import IsOwner
 from .mixins import StoryMixin
 from django.db.models import F, Count, Q
-import logging
 
-logger = logging.getLogger(__name__)
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
 
 
 class CategoryListCreateView(generics.ListCreateAPIView):
@@ -31,11 +30,31 @@ class CategoryListCreateView(generics.ListCreateAPIView):
     ordering_fields = ["title", "created_at"]
     ordering = ["-created_at"]
 
+    def list(self, request, *args, **kwargs):
+        logger.debug('Listing all categories')
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        logger.debug('Creating a new category')
+        return super().create(request, *args, **kwargs)
+
 
 class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = "slug"
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        logger.debug(f"Retrieving category with slug {kwargs['slug']}")
+        return super().retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        logger.debug(f"Updating category with slug {kwargs['slug']}")
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        logger.debug(f"Deleting category with slug {kwargs['slug']}")
+        return super().destroy(request, *args, **kwargs)
 
 
 class StoriesByCategoryView(generics.ListAPIView):
@@ -45,6 +64,7 @@ class StoriesByCategoryView(generics.ListAPIView):
 
     def get_queryset(self):
         category_slug = self.kwargs.get("category_slug")
+        logger.debug(f'Fetching stories for category slug: {category_slug}')
         return self.queryset.filter(category__slug=category_slug)
 
 
@@ -55,6 +75,7 @@ class StoryListCreateView(generics.ListCreateAPIView):
     serializer_class = StorySerializer
 
     def get_queryset(self):
+        logger.debug('Fetching all stories with annotations and prefetches')
         stories = (
             Story.objects.all()
             .annotate(
@@ -64,6 +85,7 @@ class StoryListCreateView(generics.ListCreateAPIView):
             .order_by("-created_at")
         )
 
+        logger.debug('Prefetching related StoryNode objects from Neo4j')
         # Prefetch related StoryNode objects from Neo4j
         story_ids = [story.id for story in stories]
         story_nodes = StoryNode.nodes.filter(story_id__in=story_ids)
@@ -73,6 +95,10 @@ class StoryListCreateView(generics.ListCreateAPIView):
             story.story_node = story_node_map.get(story.id)
 
         return stories
+
+    def create(self, request, *args, **kwargs):
+        logger.debug('Creating a new story')
+        return super().create(request, *args, **kwargs)
 
 
 class StoryRetrieveUpdateDestroyView(
@@ -84,6 +110,18 @@ class StoryRetrieveUpdateDestroyView(
     serializer_class = StorySerializer
     lookup_field = "slug"  # Use the slug for lookup
     lookup_url_kwarg = "story_slug"
+
+    def retrieve(self, request, *args, **kwargs):
+        logger.debug(f"Retrieving story with slug {kwargs['story_slug']}")
+        return super().retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        logger.debug(f"Updating story with slug {kwargs['story_slug']}")
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        logger.debug(f"Deleting story with slug {kwargs['story_slug']}")
+        return super().destroy(request, *args, **kwargs)
 
 
 class UserFeedView(generics.ListAPIView):
@@ -103,7 +141,8 @@ class UserFeedView(generics.ListAPIView):
             preferred_categories = user_settings.personal_settings.get(
                 "favorite_categories", []
             )
-            logger.debug(f"The preferred_categories: {preferred_categories} found!")
+            logger.debug(
+                f"The preferred_categories: {preferred_categories} found!")
         except UserSetting.DoesNotExist:
             logger.debug("User settings not found.")
             return Story.objects.none()
@@ -124,10 +163,10 @@ class UserInverseFeedView(generics.ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        print(f"The logged user: {user}!")
+        logger.debug(f"The logged user: {user}!")
 
         if not user.is_authenticated:
-            print("User is not authenticated.")
+            logger.debug("User is not authenticated.")
             return Story.objects.none()
 
         try:
@@ -135,14 +174,15 @@ class UserInverseFeedView(generics.ListAPIView):
             preferred_categories = user_settings.personal_settings.get(
                 "favorite_categories", []
             )
-            print(f"The preferred_categories: {preferred_categories} found!")
+            logger.debug(
+                f"The preferred_categories: {preferred_categories} found!")
         except UserSetting.DoesNotExist:
-            print("User settings not found.")
+            logger.debug("User settings not found.")
             return Story.objects.none()
 
         if "__all__" in preferred_categories:
             # Return an empty queryset if user's favorite categories include all categories
-            print("User's favorite categories include all categories.")
+            logger.debug("User's favorite categories include all categories.")
             return Story.objects.none()
         elif preferred_categories:
             # Exclude stories from preferred categories
@@ -160,6 +200,7 @@ class TrendingStoriesView(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         if not user.is_authenticated:
+            logger.debug("User is not authenticated.")
             return Story.objects.none()
 
         try:
@@ -167,7 +208,11 @@ class TrendingStoriesView(generics.ListAPIView):
             favorite_categories = user_settings.personal_settings.get(
                 "favorite_categories", []
             )
+            logger.debug(
+                f"Favorite categories for user {user.id}: {favorite_categories}")
+
         except UserSetting.DoesNotExist:
+            logger.debug("User settings not found.")
             return Story.objects.none()
 
         queryset = (
@@ -189,6 +234,7 @@ class TrendingStoriesView(generics.ListAPIView):
             .order_by("-calculated_trending_score")
         )
 
+        logger.debug("Trending stories queryset prepared.")
         return queryset
 
 
@@ -199,9 +245,12 @@ class LikeCreateView(StoryMixin, generics.CreateAPIView):
 
     def perform_create(self, serializer):
         try:
+            logger.debug('Creating a like')
             serializer.save()
         except IntegrityError:
-            raise serializers.ValidationError("You have already liked this story.")
+            logger.warning('User has already liked this story')
+            raise serializers.ValidationError(
+                "You have already liked this story.")
 
 
 class DislikeCreateView(StoryMixin, generics.CreateAPIView):
@@ -211,9 +260,12 @@ class DislikeCreateView(StoryMixin, generics.CreateAPIView):
 
     def perform_create(self, serializer):
         try:
+            logger.debug('Creating a dislike')
             serializer.save()
         except IntegrityError:
-            raise serializers.ValidationError("You have already disliked this story.")
+            logger.warning('User has already disliked this story')
+            raise serializers.ValidationError(
+                "You have already disliked this story.")
 
 
 class LikeDestroyView(StoryMixin, generics.DestroyAPIView):
@@ -224,6 +276,8 @@ class LikeDestroyView(StoryMixin, generics.DestroyAPIView):
     def get_queryset(self):
         story_id = self.kwargs.get("story_id")
         story_slug = self.kwargs.get("story_slug")
+        logger.debug(
+            f"Removing like for story_id: {story_id}, story_slug: {story_slug}")
 
         if story_id:
             return self.queryset.filter(user=self.request.user, story__id=story_id)
@@ -234,6 +288,7 @@ class LikeDestroyView(StoryMixin, generics.DestroyAPIView):
         story = self.get_story()
         obj = get_object_or_404(Like, user=self.request.user, story=story)
         self.check_object_permissions(self.request, obj)
+        logger.debug(f"Removing like for story {story.id}")
         return obj
 
 
@@ -245,6 +300,8 @@ class DislikeDestroyView(StoryMixin, generics.DestroyAPIView):
     def get_queryset(self):
         story_id = self.kwargs.get("story_id")
         story_slug = self.kwargs.get("story_slug")
+        logger.debug(
+            f"Removing dislike for story_id: {story_id}, story_slug: {story_slug}")
 
         if story_id:
             return self.queryset.filter(user=self.request.user, story__id=story_id)
@@ -255,6 +312,7 @@ class DislikeDestroyView(StoryMixin, generics.DestroyAPIView):
         story = self.get_story()
         obj = get_object_or_404(Like, user=self.request.user, story=story)
         self.check_object_permissions(self.request, obj)
+        logger.debug(f"Removing dislike for story {story.id}")
         return obj
 
 
@@ -265,11 +323,13 @@ class BookmarkCreateListView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         user = self.request.user
+        logger.debug(f"Listing bookmarks for user {user.id}")
         queryset = Bookmark.objects.filter(user=user)
 
         # Get the 'category' query parameter
         category = self.request.query_params.get("category", None)
         if category is not None:
+            logger.debug(f"Filtering bookmarks by category: {category}")
             queryset = queryset.filter(bookmark_category=category)
 
         return queryset
@@ -282,6 +342,8 @@ class BookmarkCreateListView(generics.ListCreateAPIView):
 
         # Check if the story has already been bookmarked by the user
         if Bookmark.objects.filter(story_id=story_id, user=user).exists():
+            logger.warning(
+                f"User {user.id} has already bookmarked story {story_id}")
             raise serializers.ValidationError(
                 {"detail": "You have already bookmarked this story."}
             )
@@ -291,10 +353,13 @@ class BookmarkCreateListView(generics.ListCreateAPIView):
         validated_data.update({"user": user, "story_id": story_id})
 
         # Save the bookmark with the updated validated_data
+        logger.debug(
+            f"Creating bookmark for story {story_id} by user {user.id}")
         serializer.save(**validated_data)
 
     def create(self, request, *args, **kwargs):
-        response = super(BookmarkCreateListView, self).create(request, *args, **kwargs)
+        response = super(BookmarkCreateListView, self).create(
+            request, *args, **kwargs)
 
         # Check if creation was successful
         if response.status_code == status.HTTP_201_CREATED:
@@ -310,6 +375,8 @@ class BaseBookmarkView(generics.RetrieveUpdateDestroyAPIView):
 
     def get_user_bookmark(self, lookup_field, lookup_value):
         user = self.request.user
+        logger.debug(
+            f"Retrieving bookmark for user {user.id} by {lookup_field}: {lookup_value}")
         filter_kwargs = {lookup_field: lookup_value, "user": user}
         return get_object_or_404(Bookmark, **filter_kwargs)
 
@@ -317,12 +384,14 @@ class BaseBookmarkView(generics.RetrieveUpdateDestroyAPIView):
 class BookmarkRetrieveUpdateDestroyView(BaseBookmarkView):
     def get_object(self):
         bookmark_id = self.kwargs.get("bookmark_id")
+        logger.debug(f"Retrieving bookmark with id {bookmark_id}")
         return self.get_user_bookmark("pk", bookmark_id)
 
 
 class BookmarkRetrieveUpdateDestroyViewByStoryId(BaseBookmarkView):
     def get_object(self):
         story_id = self.kwargs.get("story_id")
+        logger.debug(f"Retrieving bookmark for story id {story_id}")
         return self.get_user_bookmark("story_id", story_id)
 
 
