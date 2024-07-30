65bd9913401c451f6658c70ce5e971f1e7e13167
Muhammed Shittu <devshittu@gmail.com>
2024-07-30 01:25:50 +0100

    feat(logging): Add logging to ReportManager methods

--- feedback/managers.py
+++ feedback/managers.py
@@ -1,8 +1,17 @@
+import logging
 from django.db import models
+
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
+
 
 class ReportManager(models.Manager):
     def unflagged(self):
+        logger.debug('Retrieving unflagged reports')
         return self.filter(is_flagged=False)
 
     def flagged(self):
+        logger.debug('Retrieving flagged reports')
         return self.filter(is_flagged=True)
+
+# feedback/managers.py
