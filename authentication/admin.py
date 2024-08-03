521c09cbd34a9cb5fd1ce5651867212b9e6b5afb
Muhammed Shittu <devshittu@gmail.com>
2024-08-03 14:43:04 +0100

    feat(logging): Add detailed logging to CustomUserAdmin

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,8 +1,13 @@
+import logging
 from django.contrib import admin
 from .models import CustomUser
 
+# Set up the logger for this module
+logger = logging.getLogger('app_logger')
+
 
 class CustomUserAdmin(admin.ModelAdmin):
+    logger.debug('Initializing CustomUserAdmin class')
     # Fields to be displayed in the list view
     list_display = (
         "email",
@@ -18,7 +23,8 @@ class CustomUserAdmin(admin.ModelAdmin):
     search_fields = ("email", "display_name", "username")
 
     # Fields that can be used for filtering the list view
-    list_filter = ("is_active", "is_staff", "roles", "date_of_birth", "last_activity")
+    list_filter = ("is_active", "is_staff", "roles",
+                   "date_of_birth", "last_activity")
 
     # Fields to be used for editing in the list view itself
     list_editable = ("is_active", "is_staff")
@@ -42,6 +48,25 @@ class CustomUserAdmin(admin.ModelAdmin):
         ("Activity", {"fields": ("last_activity",)}),
     )
 
+    def save_model(self, request, obj, form, change):
+        if change:
+            logger.info(
+                f'Updating CustomUser {obj.id} by admin {request.user.id}')
+        else:
+            logger.info(f'Creating new CustomUser by admin {request.user.id}')
+        super().save_model(request, obj, form, change)
+
+    def delete_model(self, request, obj):
+        logger.warning(
+            f'Deleting CustomUser {obj.id} by admin {request.user.id}')
+        super().delete_model(request, obj)
+
+    def get_queryset(self, request):
+        logger.debug(f'Admin {request.user.id} is querying CustomUser list')
+        return super().get_queryset(request)
+
 
 # Register the model with the custom admin view
 admin.site.register(CustomUser, CustomUserAdmin)
+
+# authentication/admin.py
