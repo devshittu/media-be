99e0ec7439f6592851557d232b024da7efb0de53
Muhammed Shittu <devshittu@gmail.com>
2024-08-22 11:41:44 +0100

    Refactor: Move create_default_settings function to users/utils.py for shared access across apps

--- users/signals.py
+++ users/signals.py
@@ -1,28 +1,9 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import CustomUser, UserSetting, UserFeedPosition
+from .models import CustomUser, UserFeedPosition  # users/views.py
+from .utils import create_default_settings
 
 
-def create_default_settings(user):
-    default_settings = {
-        "system_settings": {"theme": "system", "language": "en"},
-        "account_settings": {
-            "username": user.username,
-            "display_name": user.display_name,
-            "email": user.email,
-        },
-        "notification_settings": {
-            "email": {
-                "account": True,
-                "marketing": True,
-                "updates": True,
-            },
-        },
-        "personal_settings": {
-            "favorite_categories": ["__all__"],
-        },
-    }
-    return UserSetting.objects.create(user=user, **default_settings)
 
 
 @receiver(post_save, sender=CustomUser)
