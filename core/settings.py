b97c25d8e4f2b9b3e48e4dd7a58c17fc333940b1
Muhammed Shittu <devshittu@gmail.com>
2024-09-22 07:49:18 +0100

    Modify the settings to check certificates for the elasticsearch

--- core/settings.py
+++ core/settings.py
@@ -190,8 +190,8 @@ ELASTICSEARCH_DSL = {
         'hosts': f"http://{ES_HOST}:{ES_PORT}",
         'http_auth': (ES_USERNAME, ES_PASSWORD),
         'timeout': 30,
-        'use_ssl': False,
-        'verify_certs': False,
+        'use_ssl': True,
+        'verify_certs': True,
     },
 }
 
